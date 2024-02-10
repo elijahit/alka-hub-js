dd90f1bc19d6a94320d055aa14c063056cde76b2
Gabriele Mario Tosto <gabriele.tosto@outlook.com>
2024-02-10 13:32:02 +0100

    ADD: Funzione per leggere i comandi dentro le cartelle

--- app.js
+++ app.js
@@ -1,8 +1,34 @@
-const {Client, Events, GatewayIntentBits, Activity, ActivityType} = require('discord.js');
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client, Collection, Events, GatewayIntentBits, ActivityType } = require('discord.js');
 const {token, presenceStatusName, botState} = require('./config.json');
 
 
 const client = new Client({intents: [GatewayIntentBits.Guilds]});
+
+client.commands = new Collection();
+
+// FUNZIONE PER RECUPERARE LE CARTELLE DI ESEGUZIONE DEL CODICE
+function executeFolder(mainDir) {
+
+  const foldersPath = path.join(__dirname, mainDir);
+  const commandsFolder = fs.readdirSync(foldersPath);
+
+  for (const folder of commandsFolder) {
+    const commandsPath = path.join(foldersPath, folder);
+    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+    for(const file of commandFiles) {
+      const filePath = path.join(commandsPath, file);
+      const command = require(filePath);
+
+      if ('data' in command && 'execute' in command) {
+        client.commands.set(command.data.name, command);
+      }
+    }
+  }
+}
+
+executeFolder('commands');
 
 client.once(Events.ClientReady, readyClient => {
   console.clear();
@@ -17,7 +43,8 @@ client.once(Events.ClientReady, readyClient => {
   client.user.setPresence({
   activities: [{name: presenceStatusName, state: botState, type: ActivityType.Custom}],
   status: 'online'
-  })
+  });
 });
+
 
 client.login(token);
