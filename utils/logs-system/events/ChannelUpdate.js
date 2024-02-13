271d33a46a53486618c8c9eeaa35f5a27ab6724b
Gabriele Mario Tosto <gabriele.tosto@outlook.com>
2024-02-13 18:23:56 +0100

    ADD: Alcune logs in ChannelUpdate per logs_system

--- utils/logs-system/events/ChannelUpdate.js
+++ utils/logs-system/events/ChannelUpdate.js
@@ -48,6 +48,73 @@ module.exports = {
                       .setColor(0xebb734);
                     channel_logs.send({ embeds: [embedLog] });
                   }
+                  // SE LA CATEGORIA DI UN CANALE VIENE CAMBIATA
+                  if(oldChannel.parentId != newChannel.parentId) {
+                    const fields = [];
+                    if(oldChannel.parentId) {
+                      fields.push({name: `${language_result.channelUpdate.old_category}`, value: `${oldChannel.parent.name}`, inline: true }); 
+                    }
+                    else {
+                      fields.push({name: `${language_result.channelUpdate.old_category}`, value: `${language_result.channelUpdate.empty_category}`, inline: true }); 
+                    }
+                    if (newChannel.parentId) {
+                      fields.push({ name: `${language_result.channelUpdate.new_category}`, value: `${newChannel.parent.name}`, inline: true });
+                    } else {
+                      fields.push({ name: `${language_result.channelUpdate.new_category}`, value: `${language_result.channelUpdate.empty_category}`, inline: true });
+                    }
+                    fields.push({name: " ", value: " "},
+                    {name: `${language_result.channelUpdate.id_channel}`, value: `${oldChannel.id}`},
+                    {name: " ", value: " "},
+                    { name: `${language_result.channelUpdate.go_channel}`, value: `${newChannel.url}`, inline: true })
+
+                    const embedLog = new EmbedBuilder()
+                      .setAuthor({ name: `${language_result.channelUpdate.embed_title}` })
+                      .addFields(fields)         
+                      .setDescription(language_result.channelUpdate.category_change_embed)
+                      .setFooter({text: `${language_result.channelUpdate.embed_footer}`, iconURL: `${language_result.channelUpdate.embed_icon_url}`})
+                      .setColor(0xebb734);
+                    channel_logs.send({ embeds: [embedLog] });
+                  }
+                  // SE IL BITRATE VIENE CAMBIATO
+                  if (oldChannel.bitrate != newChannel.bitrate) {
+                    const fields = [{ name: `${language_result.channelUpdate.old_bitrate}`, value: `${oldChannel.bitrate}`, inline: true },
+                    { name: `${language_result.channelUpdate.new_bitrate}`, value: `${newChannel.bitrate}`, inline: true },
+                    {name: " ", value: " "},
+                    {name: `${language_result.channelUpdate.id_channel}`, value: `${oldChannel.id}`},
+                    {name: " ", value: " "},
+                    { name: `${language_result.channelUpdate.go_channel}`, value: `${newChannel.url}`, inline: true }];
+                    if (oldChannel.parentId) {
+                      fields.push({ name: `${language_result.channelUpdate.category_channel}`, value: `${newChannel.parent.name}`, inline: true });
+                    }
+
+                    const embedLog = new EmbedBuilder()
+                      .setAuthor({ name: `${language_result.channelUpdate.embed_title}` })
+                      .addFields(fields)         
+                      .setDescription(language_result.channelUpdate.bitrate_change_embed)
+                      .setFooter({text: `${language_result.channelUpdate.embed_footer}`, iconURL: `${language_result.channelUpdate.embed_icon_url}`})
+                      .setColor(0xebb734);
+                    channel_logs.send({ embeds: [embedLog] });
+                  }
+                  // SE IL LIMITE DEGLI UTENTI VIENE CAMBIATO
+                  if (oldChannel.userLimit != newChannel.userLimit) {
+                    const fields = [{ name: `${language_result.channelUpdate.old_userlimit}`, value: `${oldChannel.userLimit}`, inline: true },
+                    { name: `${language_result.channelUpdate.new_userlimit}`, value: `${newChannel.userLimit}`, inline: true },
+                    {name: " ", value: " "},
+                    {name: `${language_result.channelUpdate.id_channel}`, value: `${oldChannel.id}`},
+                    {name: " ", value: " "},
+                    { name: `${language_result.channelUpdate.go_channel}`, value: `${newChannel.url}`, inline: true }];
+                    if (oldChannel.parentId) {
+                      fields.push({ name: `${language_result.channelUpdate.category_channel}`, value: `${newChannel.parent.name}`, inline: true });
+                    }
+
+                    const embedLog = new EmbedBuilder()
+                      .setAuthor({ name: `${language_result.channelUpdate.embed_title}` })
+                      .addFields(fields)         
+                      .setDescription(language_result.channelUpdate.userlimit_change_embed)
+                      .setFooter({text: `${language_result.channelUpdate.embed_footer}`, iconURL: `${language_result.channelUpdate.embed_icon_url}`})
+                      .setColor(0xebb734);
+                    channel_logs.send({ embeds: [embedLog] });
+                  }
                 })
                 .catch((error) => {
                   errorSendControls(error, oldChannel.client, oldChannel.guild, "\\logs_system\\ChannelCreate.js");
