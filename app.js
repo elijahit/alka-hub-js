f9f0c1e23c85853b227e715bacc091913868c005
Gabriele Mario Tosto <gabriele.tosto@outlook.com>
2024-02-15 22:54:03 +0100

    FEAT: Aggiunta funzionalità GuildMemberAdd per logs_system

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const fs = require('node:fs');
 const path = require('node:path');
 const { Client, Collection, Events, GatewayIntentBits, ActivityType } = require('discord.js');
-const {token, presenceStatusName, botState} = require('./config.json');
+const { token, presenceStatusName, botState } = require('./config.json');
 
 
-const client = new Client({intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates, GatewayIntentBits.GuildModeration, GatewayIntentBits.GuildEmojisAndStickers]});
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates, GatewayIntentBits.GuildModeration, GatewayIntentBits.GuildEmojisAndStickers, GatewayIntentBits.GuildMembers] });
 
 client.commands = new Collection();
 
@@ -12,18 +12,18 @@ client.commands = new Collection();
 function executeFolderModule(mainDir) {
   const foldersPath = path.join(__dirname, mainDir);
   const moduleFolder = fs.readdirSync(foldersPath);
-  
+
   for (const folder of moduleFolder) {
     const modulePath = path.join(foldersPath, folder);
     const eventsPathResolve = `${modulePath}\\events`;
     const commandsPathResolve = `${modulePath}\\command`;
     const eventsFiles = fs.readdirSync(eventsPathResolve).filter(file => file.endsWith('.js'));
     const commandFiles = fs.readdirSync(commandsPathResolve).filter(file => file.endsWith('.js'));
-    for(const file of commandFiles) {
+    for (const file of commandFiles) {
       try {
         const filePath = path.join(commandsPathResolve, file);
         const command = require(filePath);
-        
+
         if ('data' in command && 'execute' in command) {
           client.commands.set(command.data.name, command);
           console.log(`[C] ${file} caricato`);
@@ -51,25 +51,25 @@ function executeFolderModule(mainDir) {
 // Questo EVENT LISTNER serve per ascoltare i comandi che vengono lanciati con (/) slashCommands dopo averli ascoltati li invierà all'esecutore che è presente nei file dei comandi.
 
 client.on(Events.InteractionCreate, async interaction => {
-	if (!interaction.isChatInputCommand()) return;
+  if (!interaction.isChatInputCommand()) return;
 
-	const command = interaction.client.commands.get(interaction.commandName);
+  const command = interaction.client.commands.get(interaction.commandName);
 
-	if (!command) {
-		console.error(`[WARNING] Il comando ${interaction.commandName} lanciato da ${interaction.user.username} non è stato trovato.`);
-		return;
-	}
+  if (!command) {
+    console.error(`[WARNING] Il comando ${interaction.commandName} lanciato da ${interaction.user.username} non è stato trovato.`);
+    return;
+  }
 
-	try {
-		await command.execute(interaction);
-	} catch (error) {
-		console.error(error);
-		if (interaction.replied || interaction.deferred) {
-			await interaction.followUp({ content: 'Abbiamo riscontrato un errore eseguendo questo comando! Contatta un amministratore.', ephemeral: true });
-		} else {
-			await interaction.reply({ content: 'Abbiamo riscontrato un errore eseguendo questo comando! Contatta un amministratore.', ephemeral: true });
-		}
-	}
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    if (interaction.replied || interaction.deferred) {
+      await interaction.followUp({ content: 'Abbiamo riscontrato un errore eseguendo questo comando! Contatta un amministratore.', ephemeral: true });
+    } else {
+      await interaction.reply({ content: 'Abbiamo riscontrato un errore eseguendo questo comando! Contatta un amministratore.', ephemeral: true });
+    }
+  }
 });
 
 // EVENT LISTNER PER AVVIO DEL BOT
@@ -87,8 +87,8 @@ client.once(Events.ClientReady, readyClient => {
 
 
   client.user.setPresence({
-  activities: [{name: presenceStatusName, state: botState, type: ActivityType.Custom}],
-  status: 'online'
+    activities: [{ name: presenceStatusName, state: botState, type: ActivityType.Custom }],
+    status: 'online'
   });
 });
 
