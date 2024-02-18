38b90c6366154ceca614b95d964a3a489df0b0c7
Gabriele Mario Tosto <gabriele.tosto@outlook.com>
2024-02-18 21:51:28 +0100

    REFACT: Refactoring GuildBanRemove.js nella gestione delle promise

--- utils/logs-system/events/GuildBanRemove.js
+++ utils/logs-system/events/GuildBanRemove.js
@@ -2,7 +2,7 @@ const { Events, EmbedBuilder, TextChannel } = require('discord.js');
 const { readFileSync } = require('fs');
 const language = require('../../../languages/languages');
 const database = require('../../../bin/database');
-const { errorSendControls } = require('../../../bin/HandlingFunctions');
+const { errorSendControls, getEmojifromUrl } = require('../../../bin/HandlingFunctions');
 
 // QUERY DEFINITION
 let sqlChannelId_log = `SELECT guildBanState_channel FROM log_system_config WHERE guildId = ?`;
@@ -12,65 +12,56 @@ let sqlEnabledFeature = `SELECT logSystem_enabled FROM guilds_config WHERE guild
 module.exports = {
   name: Events.GuildBanRemove,
   async execute(ban) {
+    let customEmoji = await getEmojifromUrl(ban.guild.client, "unban");
     // CONTROLLO SE LA FUNZIONE E' ABILITATA
-    database.db.get(sqlEnabledFeature, [ban.guild.id], (_, result_Db) => {
+    database.getValueDatabase(sqlEnabledFeature, ban.guild.id, (result_Db) => {
       if (!result_Db) return;
       if (result_Db.logSystem_enabled != 1) return;
       // CERCO L'ID DEL CANALE DI LOG NEL DATABASE
-      database.db.get(sqlChannelId_log, [ban.guild.id], (_, result) => {
-        if (!result?.guildBanState_channel) return;
-        if (result.guildBanState_channel?.length < 5) return;
-        // CONTROLLO DELLA LINGUA
-        if (ban.guild?.id) {
-          language.databaseCheck(ban.guild.id)
-            .then(data => {
-              const langagues_path = readFileSync(`./languages/logs_system/${data}.json`);
-              const language_result = JSON.parse(langagues_path);
-
-              ban.guild.channels.fetch(result.guildBanState_channel)
-                .then(channel_logs => {
-                  const fields = [];
-                  fields.push(
-                    {name: `${language_result.guildBanRemove.ban_user}`, value: `${ban.user}`, inline: true},
-                    {name: `${language_result.guildBanRemove.ban_user_id}`, value: `${ban.user.id}`, inline: true},
-                    {name: " ", value: " "},
-                    {name: `${language_result.guildBanRemove.ban_username}`, value: `${ban.user.username}`, inline: true}
-                  );
-
-                  if (ban.user.bot) {
-                    fields.push({name: `${language_result.guildBanRemove.ban_bot_embed}`, value: `${language_result.guildBanRemove.ban_bot_response}`, inline: true});
-                  }
-
-                  if (ban.reason) {
-                    fields.push({name: `${language_result.guildBanRemove.ban_reason_embed}`, value: `${ban.reason}`});
-                  }
-                  else {
-                    fields.push({name: ` `, value: ` `});
-                  }
-
-                  setTimeout(() => {
-                    const embedLog = new EmbedBuilder()
-                      .setAuthor({ name: `${language_result.guildBanRemove.ban_title}` })
-                      .addFields(fields)
-                      .setFooter({ text: `${language_result.guildBanRemove.ban_footer}`, iconURL: `${language_result.guildBanRemove.ban_icon_url}` })
-                      .setDescription(language_result.guildBanRemove.ban_remove)
-                      .setColor(0x368c29);
-                    if(ban.user.avatar) {
-                      embedLog.setThumbnail(`https://cdn.discordapp.com/avatars/${ban.user.id}/${ban.user.avatar}.png`);
-                    }
-                    channel_logs.send({ embeds: [embedLog] })
-                      .catch(() => {
-                        return;
-                      });
-                  }, 2000);
-                })
-                .catch((error) => {
-                  return;
-                });
-            })
-            .catch((error) => {
-              errorSendControls(error, ban.client, ban.guild, "\\logs_system\\GuildBanRemove.js");
-            });
+      database.getValueDatabase(sqlChannelId_log, ban.guild.id, async (result) => {
+        try {
+          if (!result?.guildBanState_channel) return;
+          if (result.guildBanState_channel?.length < 5) return;
+          // CONTROLLO DELLA LINGUA
+          if (ban.guild?.id) {
+            let data = await language.databaseCheck(ban.guild.id);
+            const langagues_path = readFileSync(`./languages/logs_system/${data}.json`);
+            const language_result = JSON.parse(langagues_path);
+  
+            let channel_logs = await ban.guild.channels.fetch(result.guildBanState_channel);
+            const fields = [];
+            fields.push(
+              { name: `${language_result.guildBanRemove.ban_user}`, value: `${ban.user}`, inline: true },
+              { name: `${language_result.guildBanRemove.ban_user_id}`, value: `${ban.user.id}`, inline: true },
+              { name: " ", value: " " },
+              { name: `${language_result.guildBanRemove.ban_username}`, value: `${ban.user.username}`, inline: true }
+            );
+  
+            if (ban.user.bot) {
+              fields.push({ name: `${language_result.guildBanRemove.ban_bot_embed}`, value: `${language_result.guildBanRemove.ban_bot_response}`, inline: true });
+            }
+  
+            if (ban.reason) {
+              fields.push({ name: `${language_result.guildBanRemove.ban_reason_embed}`, value: `${ban.reason}` });
+            }
+            else {
+              fields.push({ name: ` `, value: ` ` });
+            }
+  
+            const embedLog = new EmbedBuilder()
+              .setAuthor({ name: `${language_result.guildBanRemove.ban_title}`, iconURL: customEmoji })
+              .addFields(fields)
+              .setFooter({ text: `${language_result.guildBanRemove.ban_footer}`, iconURL: `${language_result.guildBanRemove.ban_icon_url}` })
+              .setDescription(language_result.guildBanRemove.ban_remove)
+              .setColor(0x368c29);
+            if (ban.user.avatar) {
+              embedLog.setThumbnail(`https://cdn.discordapp.com/avatars/${ban.user.id}/${ban.user.avatar}.png`);
+            }
+            channel_logs.send({ embeds: [embedLog] });
+          }
+        }
+        catch (error) {
+          errorSendControls(error, ban.guil.client, ban.guild, "\\logs_system\\GuildBanRemove.js");
         }
       });
     });
