e19a0144b0f7f34cedb767899bdc34b111bc0ba6
Gabriele Mario Tosto <gabriele.tosto@outlook.com>
2024-02-18 22:20:54 +0100

    REFACT: Refactoring EmojiUpdate.js nella gestione delle promise

--- utils/logs-system/events/EmojiUpdate.js
+++ utils/logs-system/events/EmojiUpdate.js
@@ -2,7 +2,7 @@ const { Events, EmbedBuilder, TextChannel } = require('discord.js');
 const { readFileSync } = require('fs');
 const language = require('../../../languages/languages');
 const database = require('../../../bin/database');
-const { errorSendControls } = require('../../../bin/HandlingFunctions');
+const { errorSendControls, getEmojifromUrl } = require('../../../bin/HandlingFunctions');
 
 // QUERY DEFINITION
 let sqlChannelId_log = `SELECT emojiState_channel FROM log_system_config WHERE guildId = ?`;
@@ -12,81 +12,73 @@ let sqlEnabledFeature = `SELECT logSystem_enabled FROM guilds_config WHERE guild
 module.exports = {
   name: Events.GuildEmojiUpdate,
   async execute(oldEmoji, newEmoji) {
+    let customEmoji = await getEmojifromUrl(oldEmoji.client, "update");
     // CONTROLLO SE LA FUNZIONE E' ABILITATA
-    database.db.get(sqlEnabledFeature, [oldEmoji.guild.id], (_, result_Db) => {
+    database.getValueDatabase(sqlEnabledFeature, oldEmoji.guild.id, (result_Db) => {
       if (!result_Db) return;
       if (result_Db.logSystem_enabled != 1) return;
       // CERCO L'ID DEL CANALE DI LOG NEL DATABASE
-      database.db.get(sqlChannelId_log, [oldEmoji.guild.id], (_, result) => {
-        if (!result?.emojiState_channel) return;
-        if (result.emojiState_channel?.length < 5) return;
-        // CONTROLLO DELLA LINGUA
-        if (oldEmoji.guild?.id) {
-          language.databaseCheck(oldEmoji.guild.id)
-            .then(data => {
-              const langagues_path = readFileSync(`./languages/logs_system/${data}.json`);
-              const language_result = JSON.parse(langagues_path);
+      database.getValueDatabase(sqlChannelId_log, oldEmoji.guild.id, async (result) => {
+        try {
 
-              oldEmoji.guild.channels.fetch(result.emojiState_channel)
-                .then(channel_logs => {
-                  const fields = [];
-                  
-                  // CONTROLLO EMOJI ANIMATA O NO
-                  let animatedEmoji, avaliableEmoji;
-                  switch(oldEmoji.animated) {
-                    case true:
-                      animatedEmoji = language_result.emojiUpdate.emoji_animated;
-                      break;
-                    case false:
-                      animatedEmoji = language_result.emojiUpdate.emoji_not_animated;
-                  }
-
-                  // CONTROLLO EMOJI DISPONIBILE O NO
-                  switch(oldEmoji.available) {
-                    case true:
-                      avaliableEmoji = language_result.emojiUpdate.emoji_avaliable;
-                      break;
-                    case false:
-                      avaliableEmoji = language_result.emojiUpdate.emoji_not_avaliable;
-                  }
-
-                  fields.push(
-                    {name: `${language_result.emojiUpdate.emoji_name_old}`, value: `${oldEmoji.name}`, inline: true},
-                    {name: `${language_result.emojiUpdate.emoji_name_new}`, value: `${newEmoji.name}`, inline: true},
-                    {name: `${language_result.emojiUpdate.emoji_id}`, value: `${oldEmoji.id}`, inline: false},
-                    {name: ` `, value: ` `},
-                    {name: `${language_result.emojiUpdate.emoji_state}`, value: `${animatedEmoji}`, inline: true},
-                    {name: `${language_result.emojiUpdate.emoji_state_avaliable}`, value: `${avaliableEmoji}`, inline: true}
-                  );
-                  
-                  oldEmoji.guild.emojis.fetch(oldEmoji.id)
-                    .then(emoji => {
-                      fields.push({name: " ", value: `${language_result.emojiUpdate.emoji_rappresentative}: ${emoji}`});
-                    })
-                    .catch((error) => {
-                      errorSendControls(error, oldEmoji.client, oldEmoji.guild, "\\logs_system\\EmojiUpdate.js");
-                    });
-
-                  setTimeout(() => {
-                    const embedLog = new EmbedBuilder()
-                      .setAuthor({ name: `${language_result.emojiUpdate.embed_title}` })
-                      .addFields(fields)
-                      .setFooter({ text: `${language_result.emojiUpdate.embed_footer}`, iconURL: `${language_result.emojiUpdate.embed_icon_url}` })
-                      .setDescription(language_result.emojiUpdate.emoji_create)
-                      .setColor(0x1c7872);
-                    channel_logs.send({ embeds: [embedLog] })
-                      .catch(() => {
-                        return;
-                      });
-                  }, 2000);
-                })
-                .catch((error) => {
-                  return;
-                });
-            })
-            .catch((error) => {
-              errorSendControls(error, oldEmoji.client, oldEmoji.guild, "\\logs_system\\EmojiUpdate.js");
-            });
+          if (!result?.emojiState_channel) return;
+          if (result.emojiState_channel?.length < 5) return;
+          // CONTROLLO DELLA LINGUA
+          if (oldEmoji.guild?.id) {
+            let data = await language.databaseCheck(oldEmoji.guild.id);
+            const langagues_path = readFileSync(`./languages/logs_system/${data}.json`);
+            const language_result = JSON.parse(langagues_path);
+  
+            let channel_logs = await oldEmoji.guild.channels.fetch(result.emojiState_channel);
+            const fields = [];
+            
+            // CONTROLLO EMOJI ANIMATA O NO
+            let animatedEmoji, avaliableEmoji;
+            switch(oldEmoji.animated) {
+              case true:
+                animatedEmoji = language_result.emojiUpdate.emoji_animated;
+                break;
+              case false:
+                animatedEmoji = language_result.emojiUpdate.emoji_not_animated;
+            }
+  
+            // CONTROLLO EMOJI DISPONIBILE O NO
+            switch(oldEmoji.available) {
+              case true:
+                avaliableEmoji = language_result.emojiUpdate.emoji_avaliable;
+                break;
+              case false:
+                avaliableEmoji = language_result.emojiUpdate.emoji_not_avaliable;
+            }
+  
+            fields.push(
+              {name: `${language_result.emojiUpdate.emoji_name_old}`, value: `${oldEmoji.name}`, inline: true},
+              {name: `${language_result.emojiUpdate.emoji_name_new}`, value: `${newEmoji.name}`, inline: true},
+              {name: `${language_result.emojiUpdate.emoji_id}`, value: `${oldEmoji.id}`, inline: false},
+              {name: ` `, value: ` `},
+              {name: `${language_result.emojiUpdate.emoji_state}`, value: `${animatedEmoji}`, inline: true},
+              {name: `${language_result.emojiUpdate.emoji_state_avaliable}`, value: `${avaliableEmoji}`, inline: true}
+            );
+            
+            oldEmoji.guild.emojis.fetch(oldEmoji.id)
+              .then(emoji => {
+                fields.push({name: " ", value: `${language_result.emojiUpdate.emoji_rappresentative}: ${emoji}`});
+              })
+              .catch((error) => {
+                errorSendControls(error, oldEmoji.client, oldEmoji.guild, "\\logs_system\\EmojiUpdate.js");
+              });
+  
+            const embedLog = new EmbedBuilder()
+              .setAuthor({ name: `${language_result.emojiUpdate.embed_title}`, iconURL: customEmoji })
+              .addFields(fields)
+              .setFooter({ text: `${language_result.emojiUpdate.embed_footer}`, iconURL: `${language_result.emojiUpdate.embed_icon_url}` })
+              .setDescription(language_result.emojiUpdate.emoji_create)
+              .setColor(0x1c7872);
+            channel_logs.send({ embeds: [embedLog] });
+          }
+        }
+        catch (error) {
+          errorSendControls(error, oldEmoji.client, oldEmoji.guild, "\\logs_system\\EmojiUpdate.js");
         }
       });
     });
