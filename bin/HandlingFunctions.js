f872f496d280981e784dee7d5365df6c286182fb
Gabriele Mario Tosto <gabriele.tosto@outlook.com>
2024-02-20 20:07:09 +0100

    FIX: Migliorata la funzione checkHavePermissions con il controllo per admin

--- bin/HandlingFunctions.js
+++ bin/HandlingFunctions.js
@@ -61,27 +61,50 @@ async function getEmojifromUrl(client, name) {
   return `https://cdn.discordapp.com/emojis/${emoji}.webp?size=44&quality=lossless`;
 }
 
-async function checkHavePermissions(member, pex, fn) {
+function checkHavePermissions(interaction, pex) {
+  const member = interaction.member;
   // LA FUNZIONE CONTROLLA NEL DATABASE SE ALMENO UNO DEI RUOLI
   // E' PRESENTE NEL HASH DI PERMESSO
   let checkSqlRole = `SELECT * FROM rank_system_permissions WHERE guildId = ? AND roleId = ? AND hashRank = ? LIMIT 1`;
-  let check = false;
-  for (role of member.roles.cache) {
-    database.db.get(checkSqlRole, [member.guild.id, role[1].id, pex], (_, result) => {
-      if (result) {
-        if(check == false) {
-          check = true;
-          return fn(result);
+  let check = false, admin = false;
+  return new Promise((resolve) => {
+    for (role of member.roles.cache) {
+      database.db.get(checkSqlRole, [member.guild.id, role[1].id, pex], (_, result) => {
+        // la funzione .has tende a dare errore se non sei admin, quindi crasha
+        // l'ho inserito in un try per non farlo crashare e gestire i valori
+        try {
+          if (member.permissionsIn(interaction.channel).has("ADMINISTRATOR")) {
+            admin = true;
+          }
         }
-      }
-    })
-  }
+        catch {
+          admin = false;
+        }
+        if (result || admin) {
+          if (check == false) {
+            resolve(true);
+          }
+        }
+      })
+    }
+    setTimeout(() => {
+      resolve(false);
+    });
+  })
 }
 
+async function returnPermission(interaction, pex, fn) {
+  if (await checkHavePermissions(interaction, pex)) {
+    return fn(true);
+  } else {
+    return fn(false);
+  }
+}
 
 module.exports = {
   errorSendControls,
   getEmoji,
   getEmojifromUrl,
   checkHavePermissions,
+  returnPermission,
 }
