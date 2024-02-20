0b120fba923827c368e23d29a73bbd51fa39d171
Gabriele Mario Tosto <gabriele.tosto@outlook.com>
2024-02-20 23:16:58 +0100

    FIX: DeployCommands all

--- deploy-commands/all.js
+++ deploy-commands/all.js
@@ -11,7 +11,7 @@ const commandFolders = fs.readdirSync(foldersPath);
 for (const folder of commandFolders) {
 	// Grab all the command files from the commands directory you created earlier
 	const commandsPath = path.join(foldersPath, folder);
-	const commandsPathResolve = `${commandsPath}\\command`;
+	const commandsPathResolve = `${commandsPath}//command`;
 	const commandFiles = fs.readdirSync(commandsPathResolve).filter(file => file.endsWith('.js'));
 	// Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
 	for (const file of commandFiles) {
