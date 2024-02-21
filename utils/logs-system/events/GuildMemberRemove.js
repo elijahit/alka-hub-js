b7939a0554534de44025fd05dd043f14a2729055
Gabriele Mario Tosto <gabriele.tosto@outlook.com>
2024-02-21 16:58:51 +0100

    REV: Rivistato GuildMemberRemove.js in base al nuovo wrapper sqlite3

--- utils/logs-system/events/GuildMemberRemove.js
+++ utils/logs-system/events/GuildMemberRemove.js
@@ -1,7 +1,7 @@
 const { Events, EmbedBuilder, TextChannel } = require('discord.js');
-const { readFileSync } = require('fs');
+const { readFileSync, read } = require('fs');
 const language = require('../../../languages/languages');
-const database = require('../../../bin/database');
+const { readDb } = require('../../../bin/database');
 const { errorSendControls, getEmojifromUrl } = require('../../../bin/HandlingFunctions');
 
 // QUERY DEFINITION
@@ -14,50 +14,48 @@ module.exports = {
   async execute(member) {
     let customEmoji = await getEmojifromUrl(member.client, "memberremove");
     // CONTROLLO SE LA FUNZIONE E' ABILITATA
-    database.getValueDatabase(sqlEnabledFeature, member.guild.id, (result_Db) => {
-      try {
-        if (!result_Db) return;
-        if (result_Db.logSystem_enabled != 1) return;
-        // CERCO L'ID DEL CANALE DI LOG NEL DATABASE
-        database.getValueDatabase(sqlChannelId_log, member.guild.id, async (result) => {
-          if (!result?.guildMemberState_channel) return;
-          if (result.guildMemberState_channel?.length < 5) return;
-          // CONTROLLO DELLA LINGUA
-          if (member.guild?.id) {
-            let data = await language.databaseCheck(member.guild.id);
-            const langagues_path = readFileSync(`./languages/logs_system/${data}.json`);
-            const language_result = JSON.parse(langagues_path);
-  
-            let channel_logs = await member.guild.channels.fetch(result.guildMemberState_channel);
-            const fields = [];
-  
-            fields.push(
-              { name: `${language_result.guildMemberRemove.embed_user}`, value: `${member}`, inline: true },
-              { name: `${language_result.guildMemberRemove.embed_id}`, value: `${member.id}`, inline: true },
-              { name: " ", value: " " },
-              { name: `${language_result.guildMemberRemove.embed_username}`, value: `${member.user.username}`, inline: true }
-            );
-  
-            if (member.user.bot) {
-              fields.push({ name: `${language_result.guildMemberRemove.bot_embed}`, value: `${language_result.guildMemberRemove.bot_embed_response}`, inline: true });
-            }
-  
-            const embedLog = new EmbedBuilder()
-              .setAuthor({ name: `${language_result.guildMemberRemove.embed_title}`, iconURL: customEmoji })
-              .addFields(fields)
-              .setFooter({ text: `${language_result.guildMemberRemove.embed_footer}`, iconURL: `${language_result.guildMemberRemove.embed_icon_url}` })
-              .setDescription(language_result.guildMemberRemove.embed_description)
-              .setColor(0x8c2929);
-            if (member.user.avatar) {
-              embedLog.setThumbnail(`https://cdn.discordapp.com/avatars/${member.id}/${member.user.avatar}.png`);
-            }
-            channel_logs.send({ embeds: [embedLog] })
-          }
-        });
+    const result_Db = await readDb(sqlEnabledFeature, member.guild.id);
+    try {
+      if (!result_Db) return;
+      if (result_Db.logSystem_enabled != 1) return;
+      // CERCO L'ID DEL CANALE DI LOG NEL DATABASE
+      const result = await readDb(sqlChannelId_log, member.guild.id);
+      if (!result?.guildMemberState_channel) return;
+      if (result.guildMemberState_channel?.length < 5) return;
+      // CONTROLLO DELLA LINGUA
+      if (member.guild?.id) {
+        let data = await language.databaseCheck(member.guild.id);
+        const langagues_path = readFileSync(`./languages/logs_system/${data}.json`);
+        const language_result = JSON.parse(langagues_path);
+
+        let channel_logs = await member.guild.channels.fetch(result.guildMemberState_channel);
+        const fields = [];
+
+        fields.push(
+          { name: `${language_result.guildMemberRemove.embed_user}`, value: `${member}`, inline: true },
+          { name: `${language_result.guildMemberRemove.embed_id}`, value: `${member.id}`, inline: true },
+          { name: " ", value: " " },
+          { name: `${language_result.guildMemberRemove.embed_username}`, value: `${member.user.username}`, inline: true }
+        );
+
+        if (member.user.bot) {
+          fields.push({ name: `${language_result.guildMemberRemove.bot_embed}`, value: `${language_result.guildMemberRemove.bot_embed_response}`, inline: true });
+        }
+
+        const embedLog = new EmbedBuilder()
+          .setAuthor({ name: `${language_result.guildMemberRemove.embed_title}`, iconURL: customEmoji })
+          .addFields(fields)
+          .setFooter({ text: `${language_result.guildMemberRemove.embed_footer}`, iconURL: `${language_result.guildMemberRemove.embed_icon_url}` })
+          .setDescription(language_result.guildMemberRemove.embed_description)
+          .setColor(0x8c2929);
+        if (member.user.avatar) {
+          embedLog.setThumbnail(`https://cdn.discordapp.com/avatars/${member.id}/${member.user.avatar}.png`);
+        }
+        channel_logs.send({ embeds: [embedLog] })
       }
-      catch (error) {
-        errorSendControls(error, member.client, member.guild, "\\logs_system\\GuildMemberRemove.js");
-      }
-    });
+    }
+    catch (error) {
+      errorSendControls(error, member.client, member.guild, "\\logs_system\\GuildMemberRemove.js");
+    }
   },
 };
