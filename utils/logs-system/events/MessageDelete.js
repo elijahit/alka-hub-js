28cedaac93aed7c449327c8c77e27250e972a9d3
Gabriele Mario Tosto <gabriele.tosto@outlook.com>
2024-02-21 17:06:49 +0100

    REV: Rivistato MessageDelete.js in base al nuovo wrapper sqlite3

--- utils/logs-system/events/MessageDelete.js
+++ utils/logs-system/events/MessageDelete.js
@@ -1,7 +1,7 @@
 const { Events, EmbedBuilder, TextChannel } = require('discord.js');
 const { readFileSync } = require('fs');
 const language = require('../../../languages/languages');
-const database = require('../../../bin/database');
+const { readDb } = require('../../../bin/database');
 const { errorSendControls, getEmojifromUrl } = require('../../../bin/HandlingFunctions');
 
 // QUERY DEFINITION
@@ -14,59 +14,57 @@ module.exports = {
   async execute(message) {
     let customEmoji = await getEmojifromUrl(message.client, "delete");
     // CONTROLLO SE LA FUNZIONE E' ABILITATA
-    database.getValueDatabase(sqlEnabledFeature, message.guild.id, (result_Db) => {
-      if (!result_Db) return;
-      if (result_Db.logSystem_enabled != 1) return;
-      // CERCO L'ID DEL CANALE DI LOG NEL DATABASE
-      database.getValueDatabase(sqlChannelId_log, message.guild.id, async (result) => {
-        try {
+    const result_Db = await readDb(sqlEnabledFeature, message.guild.id);
+    if (!result_Db) return;
+    if (result_Db.logSystem_enabled != 1) return;
+    // CERCO L'ID DEL CANALE DI LOG NEL DATABASE
+    const result = await readDb(sqlChannelId_log, message.guild.id);
+    try {
 
-          if (!result?.messageState_channel) return;
-          if (result.messageState_channel?.length < 5) return;
-          if (message.author == message.client.user) return;
-          // CONTROLLO DELLA LINGUA
-          if (message.guild?.id) {
-            let data = await language.databaseCheck(message.guild.id);
-            const langagues_path = readFileSync(`./languages/logs_system/${data}.json`);
-            const language_result = JSON.parse(langagues_path);
-  
-            let channel_logs = await message.guild.channels.fetch(result.messageState_channel);
-            const fields = [];
+      if (!result?.messageState_channel) return;
+      if (result.messageState_channel?.length < 5) return;
+      if (message.author == message.client.user) return;
+      // CONTROLLO DELLA LINGUA
+      if (message.guild?.id) {
+        let data = await language.databaseCheck(message.guild.id);
+        const langagues_path = readFileSync(`./languages/logs_system/${data}.json`);
+        const language_result = JSON.parse(langagues_path);
 
-            fields.push(
-              {name: `${language_result.messageDelete.channel_message}`, value: `${message.channel}`, inline: true},
-              {name: `${language_result.messageDelete.channel_message_id}`, value: `${message.channelId}`, inline: true},
-              {name: " ", value: " "},
-              {name: `${language_result.messageDelete.author_message}`, value: `${message.author}`, inline: true},
-              {name: `${language_result.messageDelete.author_message_id}`, value: `${message.author.id}`, inline: true},
-              {name: " ", value: " "}
-            );
+        let channel_logs = await message.guild.channels.fetch(result.messageState_channel);
+        const fields = [];
 
-            const embedLog = new EmbedBuilder();
-            if(message.content) {
-              embedLog
-              .setDescription(`**${language_result.messageDelete.embed_description}:**\n${message.content}`);
-            }
+        fields.push(
+          { name: `${language_result.messageDelete.channel_message}`, value: `${message.channel}`, inline: true },
+          { name: `${language_result.messageDelete.channel_message_id}`, value: `${message.channelId}`, inline: true },
+          { name: " ", value: " " },
+          { name: `${language_result.messageDelete.author_message}`, value: `${message.author}`, inline: true },
+          { name: `${language_result.messageDelete.author_message_id}`, value: `${message.author.id}`, inline: true },
+          { name: " ", value: " " }
+        );
 
-            if(message.attachments.size > 0) {
-              let attachmentsContainer = "";
-              await message.attachments.each(value => {
-                attachmentsContainer += `${value.url}\n`;
-              });
-              fields.push({name: `${language_result.messageDelete.attachments_message}`, value: `${attachmentsContainer}`});
-            }
-            embedLog
-              .setAuthor({ name: `${language_result.messageDelete.embed_title}`, iconURL: customEmoji })
-              .addFields(fields)
-              .setFooter({ text: `${language_result.messageDelete.embed_footer}`, iconURL: `${language_result.messageDelete.embed_icon_url}` })
-              .setColor(0x80131e);
-            channel_logs.send({ embeds: [embedLog] });
-          }
+        const embedLog = new EmbedBuilder();
+        if (message.content) {
+          embedLog
+            .setDescription(`**${language_result.messageDelete.embed_description}:**\n${message.content}`);
         }
-        catch (error) {
-          errorSendControls(error, message.client, message.guild, "\\logs_system\\MessageDelete.js");
+
+        if (message.attachments.size > 0) {
+          let attachmentsContainer = "";
+          await message.attachments.each(value => {
+            attachmentsContainer += `${value.url}\n`;
+          });
+          fields.push({ name: `${language_result.messageDelete.attachments_message}`, value: `${attachmentsContainer}` });
         }
-      });
-    });
+        embedLog
+          .setAuthor({ name: `${language_result.messageDelete.embed_title}`, iconURL: customEmoji })
+          .addFields(fields)
+          .setFooter({ text: `${language_result.messageDelete.embed_footer}`, iconURL: `${language_result.messageDelete.embed_icon_url}` })
+          .setColor(0x80131e);
+        channel_logs.send({ embeds: [embedLog] });
+      }
+    }
+    catch (error) {
+      errorSendControls(error, message.client, message.guild, "\\logs_system\\MessageDelete.js");
+    }
   },
 };
