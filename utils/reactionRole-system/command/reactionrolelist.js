33dfd75267f90ee9283c9952e6842b142fdfd277
Gabriele Mario Tosto <gabriele.tosto@outlook.com>
2024-03-04 20:24:53 +0100

    ADD: Controllo se le stringhe sono più lunghe di quelle possibili

--- utils/reactionRole-system/command/reactionrolelist.js
+++ utils/reactionRole-system/command/reactionrolelist.js
@@ -22,12 +22,12 @@ module.exports = {
 					const checkFeaturesisEnabled = await readDb(`SELECT reactionRoleSystem_enabled from guilds_config WHERE guildId = ?`, interaction.guild.id);
 
 					const checkReactionAlreadySet = await readDbAllWith1Params(`SELECT * from reactionrole_system_reactions WHERE guildId = ?`, interaction.guild.id);
-					
+
 					const customEmoji = await getEmojifromUrl(interaction.client, "reactionrole");
 					if (checkFeaturesisEnabled?.reactionRoleSystem_enabled) {
 						reactionRoleString += `${language_result.listCommand.description_embed.replace("{0}", interaction.user)}\n\n`;
 						if (checkReactionAlreadySet?.length > 0) {
-							for(const value of checkReactionAlreadySet) {
+							for (const value of checkReactionAlreadySet) {
 								const role = await interaction.guild.roles.fetch(value.roleId);
 								const channel = await interaction.guild.channels.fetch(value.channelId);
 								const message = await channel.messages.fetch(value.messageId);
@@ -37,10 +37,23 @@ module.exports = {
 						else {
 							reactionRoleString += `\n**${language_result.listCommand.no_role}**`
 						}
+						const embedLog = new EmbedBuilder();
 
-						const embedLog = new EmbedBuilder()
+						if (reactionRoleString.length > 3000) {
+							const arrayString = reactionRoleString.split("\n");
+							let reactionRoleStringResolve = "";
+							for (const string of arrayString) {
+								if (reactionRoleStringResolve.length < 3500) {
+									reactionRoleStringResolve += `\n${string}`;
+								}
+							}
+							reactionRoleStringResolve += `\n${language_result.listCommand.most_lenght}`;
+							embedLog.setDescription(reactionRoleStringResolve);
+						} else {
+							embedLog.setDescription(reactionRoleString);
+						}
+						embedLog
 							.setAuthor({ name: `${language_result.listCommand.embed_title}`, iconURL: customEmoji })
-							.setDescription(reactionRoleString)
 							.setFooter({ text: `${language_result.listCommand.embed_footer}`, iconURL: `${language_result.listCommand.embed_icon_url}` })
 							.setColor(0x0d495c);
 						await interaction.reply({ embeds: [embedLog], ephemeral: true });
@@ -54,7 +67,8 @@ module.exports = {
 				}
 			}
 			catch (error) {
-				errorSendControls(error, interaction.client, interaction.guild, "\\reactionRole-system\\reactionrolelist.js");
+				console.log(error)
+				// errorSendControls(error, interaction.client, interaction.guild, "\\reactionRole-system\\reactionrolelist.js");
 			}
 		});
 	},
