671b82d94b2a7b1a5bbd55d1b55963eca32afecc
Gabriele Mario Tosto <gabriele.tosto@outlook.com>
2024-03-04 18:48:23 +0100

    FIX: Risolto bug che settava i ruoli al BOT in reactionrole

--- utils/reactionRole-system/events/reactionRoleRemove.js
+++ utils/reactionRole-system/events/reactionRoleRemove.js
@@ -11,41 +11,42 @@ let sqlEnabledFeature = `SELECT reactionRoleSystem_enabled FROM guilds_config WH
 module.exports = {
   name: Events.MessageReactionRemove,
   async execute(messageReaction, user) {
-
-    const message = messageReaction.message;
-    const emoji = messageReaction.emoji;
-    const guild = message.guild;
-    const member = await guild.members.fetch(user.id);
-
-
-    // CONTROLLO SE LA FUNZIONE E' ABILITATA
-    const result_Db = await readDb(sqlEnabledFeature, guild.id);
-    if (!result_Db) return;
-    if (result_Db.reactionRoleSystem_enabled != 1) return;
-
-    try {
-      let emojiResolve = "";
-      if(emoji.id) {
-        emojiResolve = `<:${emoji.name}:${emoji.id}>`;
-      } else {
-        emojiResolve = emoji.name;
-      }
-
-      const roles = await readDbWith3Params('SELECT * FROM reactionrole_system_reactions WHERE guildId = ? AND messageId = ? AND emoji = ?', guild.id, message.id, emojiResolve);
-      if(roles) {
-        try {
-          const role = await member.guild.roles.fetch(roles.roleId);
-          await member.roles.remove(role);
+    if(!user.bot) {
+      const message = messageReaction.message;
+      const emoji = messageReaction.emoji;
+      const guild = message.guild;
+      const member = await guild.members.fetch(user.id);
+  
+  
+      // CONTROLLO SE LA FUNZIONE E' ABILITATA
+      const result_Db = await readDb(sqlEnabledFeature, guild.id);
+      if (!result_Db) return;
+      if (result_Db.reactionRoleSystem_enabled != 1) return;
+  
+      try {
+        let emojiResolve = "";
+        if(emoji.id) {
+          emojiResolve = `<:${emoji.name}:${emoji.id}>`;
+        } else {
+          emojiResolve = emoji.name;
         }
-        catch (error) {
-          //pass
+  
+        const roles = await readDbWith3Params('SELECT * FROM reactionrole_system_reactions WHERE guildId = ? AND messageId = ? AND emoji = ?', guild.id, message.id, emojiResolve);
+        if(roles) {
+          try {
+            const role = await member.guild.roles.fetch(roles.roleId);
+            await member.roles.remove(role);
+          }
+          catch (error) {
+            //pass
+          }
         }
+      
+  
       }
-    
-
-    }
-    catch (error) {
-      errorSendControls(error, member.client, guild, "\\autoRole-system\\autoRoleState.js");
+      catch (error) {
+        errorSendControls(error, member.client, guild, "\\autoRole-system\\autoRoleState.js");
+      }
     }
   },
 };
