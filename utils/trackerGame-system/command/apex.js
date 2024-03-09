0d2facd801d84f80bf47ff5c464d1c3dc5334848
Gabriele Mario Tosto <gabriele.tosto@outlook.com>
2024-03-09 17:28:20 +0100

    INIT: Iniziato il comando /apex da completare

--- utils/trackerGame-system/command/apex.js
+++ utils/trackerGame-system/command/apex.js
@@ -22,12 +22,64 @@ module.exports = {
 		.setDescription('CHANGE NEXT'),
 	async execute(interaction) {
 		// RECUPERO LA LINGUA
-		try {
-			let resultApi = await checkApi("9_EazY_9", "PC");
-			console.log(resultApi)
+		let data = await language.databaseCheck(interaction.guild.id);
+		const langagues_path = readFileSync(`./languages/trackerGame-system/${data}.json`);
+		const language_result = JSON.parse(langagues_path);
 
-			if(typeof resultApi == Object) {
+		try {
+			await interaction.deferReply();
+			let resultApi = await checkApi("iiiTzEazY", "PC");
+			if (typeof resultApi == 'object') {
+				let customEmoji = await getEmojifromUrl(interaction.client, "apexlegends");
+
+				// API CHECK
+				let currentState = resultApi.data.realtime.currentState == "online" ? "🟢" : "🔴";
 				
+				// CHECK WIN / KILL / DAMAGE
+				let globalKill = 0;
+				let globalDamage = 0;
+				let globalWin = 0;
+				for await (const [_, result] of Object.entries(resultApi.data.total)) {
+					if(result.name.includes("kills") || result.name.includes("Kills")) {
+						globalKill += result.value;
+					}
+					if(result.name.includes("Damage") || result.name.includes("damage")) {
+						globalDamage += result.value;
+					}
+					if(result.name.includes("Wins") || result.name.includes("wins")) {
+						globalWin += result.value;
+					}
+				}
+				// ------------------- //
+				
+				// CHECK BEST LEGENDS
+				let legendsKill = new Map();
+				for await (const [key, result] of Object.entries(resultApi.data.legends.all)) {
+					if(result.data) {
+						await result.data.forEach(value => {
+							if(value.name.includes("Kills")) {
+								legendsKill.set(key, kill+value.value);
+							}
+						})
+					}
+				}
+				console.log(legendsKill)
+			
+
+				let fields = [
+					{name: language_result.apexTracker.username, value: `${resultApi.data.global.tag}${resultApi.data.global.name} ${currentState}`, inline: true},
+					{name: language_result.apexTracker.uid, value: `${resultApi.data.global.uid}`, inline: true},
+					{name: language_result.apexTracker.level, value: `${resultApi.data.global.level} (${resultApi.data.global.toNextLevelPercent}%)`, inline: true},
+					{name: " ", value: " "},
+					{name: language_result.apexTracker.scoreBoard, value: `${globalKill} / ${globalWin} / ${globalDamage}`, inline: true}
+				];
+				const embedLog = new EmbedBuilder()
+					.setAuthor({ name: `${language_result.apexTracker.embed_title}`, iconURL: customEmoji })
+					.addFields(fields)
+					.setFooter({ text: `${language_result.apexTracker.embed_footer}`, iconURL: `${language_result.apexTracker.embed_icon_url}` })
+					.setThumbnail(resultApi.data.global.avatar)
+					.setColor(0x318f22);
+				interaction.editReply({ embeds: [embedLog] });
 			}
 		}
 		catch (error) {
