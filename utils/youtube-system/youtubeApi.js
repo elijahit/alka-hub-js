d78ffb3665b1a5f523190707bce3e85211646efd
Gabriele Mario Tosto <gabriele.tosto@outlook.com>
2024-03-13 22:19:36 +0100

    ADD: Youtube Notify System

--- utils/youtube-system/youtubeApi.js
+++ utils/youtube-system/youtubeApi.js
@@ -44,82 +44,87 @@ youtubeServer.get('/youtubeListener', (request, reply) => {
 
 
 youtubeServer.post('/youtubeListener', async (request, reply) => {
+  reply.code(204)
+  reply.send('ok')
   if (request.body?.feed?.title) {
 
     const videoId = request.body.feed.entry['yt:videoId'];
     await postResponse(videoId);
     
   }
-  reply.code(204)
-  reply.send('ok')
 
 })
 
 async function postResponse (videoId = "string") {
-  const apiVideoResult = await youtube.videos.list({
-    part: "snippet",
-    id: videoId
-  });
-  const videoResult = Object.freeze({
-    videoId: apiVideoResult.data.items[0].id,
-    videoUrl: `https://www.youtube.com/watch?v=${apiVideoResult.data.items[0].id}`,
-    channelName: apiVideoResult.data.items[0].snippet.channelTitle,
-    title: apiVideoResult.data.items[0].snippet.title,
-    channelId: apiVideoResult.data.items[0].snippet.channelId,
-    channelUrl: `https://www.youtube.com/channel/${apiVideoResult.data.items[0].snippet.channelId}`,
-    thumbnail: apiVideoResult.data.items[0].snippet.thumbnails.standard.url,
-    description: apiVideoResult.data.items[0].snippet.localized.description,
-  })
-
-  const apiChannelResult = await youtube.channels.list({
-    part: "snippet",
-    id: videoResult.channelId
-  });
-
-  const channelResult = Object.freeze({
-    thumbnail: apiChannelResult.data.items[0].snippet.thumbnails.default.url,
-  })
-
-  const youtubeNotify = await readDbAllWith1Params("SELECT * FROM youtube_notify_system WHERE youtuberId = ?", videoResult.channelId);
-
-
-  for await (const value of youtubeNotify) {
-    // VERIFICO LA LINGUA
-    const guild = await client.guilds.fetch(value.guildId);
-    let data = await language.databaseCheck(guild.id);
-    const langagues_path = readFileSync(`./languages/youtube-system/${data}.json`);
-    const language_result = JSON.parse(langagues_path);
-
-    let videoDescription = "";
-    if (videoResult.description.length > 400) {
-      videoDescription = `${videoResult.description.slice(0, 400)}...`;
-    } else if (videoResult.description.length == 0) {
-      videoDescription = language_result.youtubeEmbed.embed_description_empty;
-    } else {
-      videoDescription = videoResult.description;
+  try {
+    const apiVideoResult = await youtube.videos.list({
+      part: "snippet",
+      id: videoId
+    });
+    const videoResult = Object.freeze({
+      videoId: apiVideoResult.data.items[0].id,
+      videoUrl: `https://www.youtube.com/watch?v=${apiVideoResult.data.items[0].id}`,
+      channelName: apiVideoResult.data.items[0].snippet.channelTitle,
+      title: apiVideoResult.data.items[0].snippet.title,
+      channelId: apiVideoResult.data.items[0].snippet.channelId,
+      channelUrl: `https://www.youtube.com/channel/${apiVideoResult.data.items[0].snippet.channelId}`,
+      thumbnail: apiVideoResult.data.items[0].snippet.thumbnails.standard.url,
+      description: apiVideoResult.data.items[0].snippet.localized.description,
+    })
+  
+    const apiChannelResult = await youtube.channels.list({
+      part: "snippet",
+      id: videoResult.channelId
+    });
+  
+    const channelResult = Object.freeze({
+      thumbnail: apiChannelResult.data.items[0].snippet.thumbnails.default.url,
+    })
+  
+    const youtubeNotify = await readDbAllWith1Params("SELECT * FROM youtube_notify_system WHERE youtuberId = ?", videoResult.channelId);
+  
+  
+    for await (const value of youtubeNotify) {
+      // VERIFICO LA LINGUA
+      const guild = await client.guilds.fetch(value.guildId);
+      let data = await language.databaseCheck(guild.id);
+      const langagues_path = readFileSync(`./languages/youtube-system/${data}.json`);
+      const language_result = JSON.parse(langagues_path);
+  
+      let videoDescription = "";
+      if (videoResult.description.length > 400) {
+        videoDescription = `${videoResult.description.slice(0, 400)}...`;
+      } else if (videoResult.description.length == 0) {
+        videoDescription = language_result.youtubeEmbed.embed_description_empty;
+      } else {
+        videoDescription = videoResult.description;
+      }
+  
+      let fields = [
+        { name: " ", value: `**[${videoResult.title}](${videoResult.videoUrl})**` },
+        { name: language_result.youtubeEmbed.embed_description, value: `${videoDescription}` }
+      ];
+  
+      let customEmoji = await getEmojifromUrl(client, "youtube");
+      const embedLog = new EmbedBuilder()
+        .setAuthor({ name: `${language_result.youtubeEmbed.embed_title}`, iconURL: customEmoji })
+        .setDescription(language_result.youtubeEmbed.description.replace("{0}", videoResult.channelName))
+        .setFooter({ text: `${language_result.youtubeEmbed.embed_footer}`, iconURL: `${language_result.youtubeEmbed.embed_icon_url}` })
+        .setFields(fields)
+        .setImage(videoResult.thumbnail)
+        .setThumbnail(channelResult.thumbnail)
+        .setColor(0x7d1a13);
+      const channel = await guild.channels.fetch(value.channelId);
+      if (value.roleMention) {
+        const role = await guild.roles.fetch(value.roleMention);
+        await channel.send({ content: `${role}`, embeds: [embedLog] });
+      } else {
+        await channel.send({ embeds: [embedLog] });
+      }
     }
-
-    let fields = [
-      { name: " ", value: `**[${videoResult.title}](${videoResult.videoUrl})**` },
-      { name: language_result.youtubeEmbed.embed_description, value: `${videoDescription}` }
-    ];
-
-    let customEmoji = await getEmojifromUrl(client, "youtube");
-    const embedLog = new EmbedBuilder()
-      .setAuthor({ name: `${language_result.youtubeEmbed.embed_title}`, iconURL: customEmoji })
-      .setDescription(language_result.youtubeEmbed.description.replace("{0}", videoResult.channelName))
-      .setFooter({ text: `${language_result.youtubeEmbed.embed_footer}`, iconURL: `${language_result.youtubeEmbed.embed_icon_url}` })
-      .setFields(fields)
-      .setImage(videoResult.thumbnail)
-      .setThumbnail(channelResult.thumbnail)
-      .setColor(0x7d1a13);
-    const channel = await guild.channels.fetch(value.channelId);
-    if (value.roleMention) {
-      const role = await guild.roles.fetch(value.roleMention);
-      await channel.send({ content: `${role}`, embeds: [embedLog] });
-    } else {
-      await channel.send({ embeds: [embedLog] });
-    }
+  }
+  catch {
+    await postResponse(videoId);
   }
 }
 
