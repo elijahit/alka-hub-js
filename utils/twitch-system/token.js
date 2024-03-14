8c6635a560ba5f3ccea8dc0c2ae11f5b77a41494
Gabriele Mario Tosto <gabriele.tosto@outlook.com>
2024-03-14 20:34:34 +0100

    FIX: Fixato il problema di aggiornamento token Twitch System

--- utils/twitch-system/token.js
+++ utils/twitch-system/token.js
@@ -3,23 +3,27 @@ const { readFileSync, writeFileSync } = require('fs');
 
 const clientId = 'ehul2uguodzxhb1jvn4xrf1lzmh94d';
 const clientSecret = 'w7bvv8adnu3fhoswjcjct4gohmos94';
-const tokenData = JSON.parse(readFileSync('./utils/twitch-system/tokens.json', 'UTF-8'));
+let tokenData = JSON.parse(readFileSync('./utils/twitch-system/tokens.json', 'UTF-8'));
 const authProvider = new RefreshingAuthProvider(
 	{
 		clientId,
 		clientSecret
 	}
 );
+authProvider.onRefresh(async (_, newTokenData) => {
+  writeFileSync(`./utils/twitch-system/tokens.json`, JSON.stringify(newTokenData, null, 4), 'UTF-8')
+  tokenData = JSON.parse(readFileSync('./utils/twitch-system/tokens.json', 'UTF-8'));
+});
 
-async function tokenRefresh() {
-  authProvider.onRefresh(async (_, newTokenData) => writeFileSync(`./utils/twitch-system/tokens.json`, JSON.stringify(newTokenData, null, 4), 'UTF-8'));
-  setInterval(async () => {
-    await tokenRefresh();
-  }, 3600000);
-}
-
-tokenRefresh();
 authProvider.addUserForToken(tokenData);
+
+// QUESTO INTERVALLO CONTROLLA SECONDO DOPO SECONDO IL TOKEN PER AGGIORNARLO QUANDO SCADE
+setInterval(async () => {
+  let expiresTime = (tokenData.obtainmentTimestamp + (tokenData.expiresIn*1000));
+  if(Date.now() >= expiresTime) {
+    await authProvider.addUserForToken(tokenData);
+  }
+}, 1000);
 
 const token = tokenData.accessToken;
 
