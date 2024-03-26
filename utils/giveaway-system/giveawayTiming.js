bf679ea83ab38a31309f698906ea17a869a30a56
Gabriele Mario Tosto <gabriele.tosto@outlook.com>
2024-03-26 23:11:00 +0100

    ADD: Aggiunto comando /giveawayend in giveawaySystem

--- utils/giveaway-system/giveawayTiming.js
+++ utils/giveaway-system/giveawayTiming.js
@@ -51,6 +51,61 @@ async function theWinnersCheker(winnersCount, checkPartecipants) {
   }
 }
 
+async function endGiveaway(value, language_result) {
+  checkPartecipants = await readDbAllWith3Params('SELECT * FROM giveaway_system_partecipants WHERE guildId = ? AND channelId = ? AND messageId = ?', value.guildId, value.channelId, value.messageId);
+  let winnersCount = value.winners;
+
+  let theWinners = await theWinnersCheker(winnersCount, checkPartecipants);
+  let guild;
+
+  // CONTROLLO SE LA GUILDS ESISTE ALTRIMENTI ELIMINO I VALORI
+  guild = await client.guilds.fetch(value.guildId);
+  try {
+    let theWinnersString = "";
+    let channel = await guild.channels.fetch(value.channelId);
+    let message = await channel.messages.fetch(value.messageId);
+    const customEmoji = await getEmojifromUrl(client, "giveaway");
+    if (theWinners) {
+      for await (const value of theWinners) {
+        theWinnersString += `<@${value}>\n`;
+        try {
+          let user = await guild.members.fetch(value);
+          const embedLog = new EmbedBuilder()
+            .setAuthor({ name: `${language_result.giveawayUserContact.embed_title}`, iconURL: customEmoji })
+            .setDescription(language_result.giveawayUserContact.description_embed
+              .replace("{0}", `${message.url}`))
+            .setFooter({ text: `${language_result.giveawayUserContact.embed_footer}`, iconURL: `${language_result.giveawayUserContact.embed_icon_url}` })
+            .setColor(0xa22297);
+          await user.send({ embeds: [embedLog] });
+        }
+        catch (error) {
+          console.log(error)
+          //Passa
+        }
+      }
+    }
+
+    // MODIFICO IL MESSAGGIO ASSEGNANDOGLI IL VINCITORE
+    const embedLog = new EmbedBuilder()
+      .setAuthor({ name: `${language_result.giveawayStart.embed_title}`, iconURL: customEmoji })
+      .setDescription(language_result.giveawayStart.description_embed
+        .replace("{0}", value.prizes)
+        .replace("{1}", value.endDate)
+        .replace("{2}", value.slots > 0 ? `${value.slots}` : language_result.giveawayStart.slotsInfinity)
+        .replace("{3}", `${value.winners}`))
+      .setFooter({ text: `${language_result.giveawayStart.embed_footer} | ID: ${value.ID}`, iconURL: `${language_result.giveawayStart.embed_icon_url}` })
+      .addFields([{ name: language_result.giveawayStart.embed_winners, value: (theWinners != null ? theWinnersString : language_result.giveawayStart.noWinners) }])
+      .setColor(0xa22297);
+    await message.edit({ embeds: [embedLog], components: [] });
+
+    await runDb("DELETE FROM giveaway_system_container WHERE guildId = ? AND messageId = ?", value.guildId, value.messageId);
+    await runDb("DELETE FROM giveaway_system_partecipants WHERE guildId = ? AND messageId = ?", value.guildId, value.messageId);
+  } catch (error) {
+
+    await runDb("DELETE FROM giveaway_system_partecipants WHERE guildId = ? AND messageId = ?", value.guildId, value.messageId);
+  }
+}
+
 async function checkGiveawayTiming() {
   checkDatabase = await readDbAll('giveaway_system_container');
   for (const value of checkDatabase) {
@@ -59,71 +114,20 @@ async function checkGiveawayTiming() {
       let data = await language.databaseCheck(value.guildId);
       const langagues_path = readFileSync(`./languages/giveaway-system/${data}.json`);
       const language_result = JSON.parse(langagues_path);
+    try {
+      await endGiveaway(value, language_result);
 
-      checkPartecipants = await readDbAllWith3Params('SELECT * FROM giveaway_system_partecipants WHERE guildId = ? AND channelId = ? AND messageId = ?', value.guildId, value.channelId, value.messageId);
-      let winnersCount = value.winners;
+    } catch (error) {
 
-      let theWinners = await theWinnersCheker(winnersCount, checkPartecipants);
-      let guild;
-
-      // CONTROLLO SE LA GUILDS ESISTE ALTRIMENTI ELIMINO I VALORI
-      try {
-        guild = await client.guilds.fetch(value.guildId);
-        // CONTROLLO SE L'UTENTE ESISTE ALTRIMENTI LO ELIMINO
-        try {
-          let theWinnersString = "";
-          let channel = await guild.channels.fetch(value.channelId);
-          let message = await channel.messages.fetch(value.messageId);
-          const customEmoji = await getEmojifromUrl(client, "giveaway");
-          if (theWinners) {
-            for await (const value of theWinners) {
-              theWinnersString += `<@${value}>\n`;
-              try {
-                let user = await guild.members.fetch(value);
-                const embedLog = new EmbedBuilder()
-                  .setAuthor({ name: `${language_result.giveawayUserContact.embed_title}`, iconURL: customEmoji })
-                  .setDescription(language_result.giveawayUserContact.description_embed
-                    .replace("{0}", `${message.url}`))
-                  .setFooter({ text: `${language_result.giveawayUserContact.embed_footer}`, iconURL: `${language_result.giveawayUserContact.embed_icon_url}` })
-                  .setColor(0xa22297);
-                await user.send({ embeds: [embedLog] });
-              }
-              catch (error) {
-                console.log(error)
-                //Passa
-              }
-            }
-          }
-
-          // MODIFICO IL MESSAGGIO ASSEGNANDOGLI IL VINCITORE
-          const embedLog = new EmbedBuilder()
-            .setAuthor({ name: `${language_result.giveawayStart.embed_title}`, iconURL: customEmoji })
-            .setDescription(language_result.giveawayStart.description_embed
-              .replace("{0}", value.prizes)
-              .replace("{1}", value.endDate)
-              .replace("{2}", value.slots > 0 ? `${value.slots}` : language_result.giveawayStart.slotsInfinity)
-              .replace("{3}", `${value.winners}`))
-            .setFooter({ text: `${language_result.giveawayStart.embed_footer} | ID: ${value.ID}`, iconURL: `${language_result.giveawayStart.embed_icon_url}` })
-            .addFields([{ name: language_result.giveawayStart.embed_winners, value: (theWinners != null ? theWinnersString : language_result.giveawayStart.noWinners) }])
-            .setColor(0xa22297);
-          await message.edit({ embeds: [embedLog], components: [] });
-
-          await runDb("DELETE FROM giveaway_system_container WHERE guildId = ? AND messageId = ?", value.guildId, value.messageId);
-          await runDb("DELETE FROM giveaway_system_partecipants WHERE guildId = ? AND messageId = ?", value.guildId, value.messageId);
-        } catch (error) {
-          
-          await runDb("DELETE FROM giveaway_system_partecipants WHERE guildId = ? AND messageId = ?", value.guildId, value.messageId);
-        }
-      } catch (error) {
-        
-        await runDb("DELETE FROM giveaway_system_container WHERE guildId = ? AND messageId = ?", value.guildId, value.messageId);
-        await runDb("DELETE FROM giveaway_system_partecipants WHERE guildId = ? AND messageId = ?", value.guildId, value.messageId);
-      }
+      await runDb("DELETE FROM giveaway_system_container WHERE guildId = ? AND messageId = ?", value.guildId, value.messageId);
+      await runDb("DELETE FROM giveaway_system_partecipants WHERE guildId = ? AND messageId = ?", value.guildId, value.messageId);
     }
   }
+}
 }
 
 module.exports = {
   endDateCheck,
   checkGiveawayTiming,
+  endGiveaway,
 }
