6afb86d20d70cf13a3d362aa363334477ae5744b
Gabriele Mario Tosto <gabriele.tosto@outlook.com>
2024-03-27 23:31:09 +0100

    EDIT: Modificato il comando /help e la sua lingua it/en

--- utils/general/command/help.js
+++ utils/general/command/help.js
@@ -7,36 +7,83 @@ const { errorSendControls, getEmojifromUrl, returnPermission, noInitGuilds, noHa
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('help')
-		.setDescription('Command to view the complete list of commands in the server'),
+		.setDescription('Command to view the complete list of commands in the server')
+		.addStringOption(value =>
+			value
+				.setName('module')
+				.setDescription('Select a module to view its help page')
+				.addChoices({
+					name: "General Command",
+					value: "general",
+				})
+				.addChoices({
+					name: "Logs System",
+					value: "logs",
+				})
+				.addChoices({
+					name: "Admin Ranks",
+					value: "permissions",
+				})
+				.addChoices({
+					name: "Ticket System",
+					value: "ticket",
+				})
+				.addChoices({
+					name: "Auto Voice System",
+					value: "autovoice",
+				})
+				.addChoices({
+					name: "Auto Role System",
+					value: "autorole",
+				})
+				.addChoices({
+					name: "Reaction Role System",
+					value: "reactionrole",
+				})
+				.addChoices({
+					name: "Statistics System",
+					value: "statistics",
+				})
+				.addChoices({
+					name: "Twitch Notify System",
+					value: "twitch",
+				})
+				.addChoices({
+					name: "YouTube Notify System",
+					value: "youtube",
+				})
+				.addChoices({
+					name: "Music Bot System",
+					value: "musicbot",
+				})
+				.addChoices({
+					name: "Giveaway System",
+					value: "giveaway",
+				})
+		),
 	async execute(interaction) {
+		let moduleSelect = await interaction.options.get("module")?.value;
 		// RECUPERO LA LINGUA
 		try {
 			let data = await language.databaseCheck(interaction.guild.id);
 			const langagues_path = readFileSync(`./languages/general/${data}.json`);
 			const language_result = JSON.parse(langagues_path);
 
-			const fields = [];
-			// COMMANDS
-			fields.push(
-				{name: language_result.helpCommand.general_category, value: language_result.helpCommand.general_commands},
-				{name: language_result.helpCommand.logs_category, value: language_result.helpCommand.logs_commands},
-				{name: language_result.helpCommand.admin_category, value: language_result.helpCommand.admin_commands},
-				{name: language_result.helpCommand.ticket_category, value: language_result.helpCommand.ticket_commands},
-				{name: language_result.helpCommand.autovoice_category, value: language_result.helpCommand.autovoice_commands},
-				{name: language_result.helpCommand.autorole_category, value: language_result.helpCommand.autorole_commands},
-				{name: language_result.helpCommand.reactionrole_category, value: language_result.helpCommand.reactionrole_commands},
-				{name: language_result.helpCommand.statistics_category, value: language_result.helpCommand.statistics_commands},
-				{name: language_result.helpCommand.twitch_category, value: language_result.helpCommand.twitch_commands},
-				{name: language_result.helpCommand.youtube_category, value: language_result.helpCommand.youtube_commands}
-			);
-
-			// FOOTER
-			fields.push({name: " ", value: language_result.helpCommand.footer});
-			
+			let fields = [];
+			const embedLog = new EmbedBuilder();
+			if(moduleSelect) {
+				fields = [
+					{name: language_result.helpCommand[`${moduleSelect}_category`], value: language_result.helpCommand[`${moduleSelect}_commands`]},
+					{name: " ", value: language_result.helpCommand.documentationRefer
+				.replace('{0}', moduleSelect)}
+				];
+				embedLog.setDescription(language_result.helpCommand.description_embed);
+			} else {
+				fields = [{name: language_result.helpCommand.noModuleSelectTitle, value: language_result.helpCommand.noModuleSelectEmbed}]
+			}
 			const customEmoji = await getEmojifromUrl(interaction.client, "help");
-			const embedLog = new EmbedBuilder()
+			embedLog
 				.setAuthor({ name: `${language_result.helpCommand.embed_title}`, iconURL: customEmoji })
-				.setDescription(language_result.helpCommand.description_embed)
 				.addFields(fields)
 				.setFooter({ text: `${language_result.helpCommand.embed_footer}`, iconURL: `${language_result.helpCommand.embed_icon_url}` })
 				.setColor(0x3262a8);
