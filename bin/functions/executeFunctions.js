33b237c5210ca7a7d3af925b240369d7f2d28882
Elijah <64620855+elijahit@users.noreply.github.com>
2024-09-04 08:30:34 +0200

    REWORK: App.js

---
+++ bin/functions/executeFunctions.js
@@ -0,0 +1,41 @@
+const fs = require('node:fs');
+const path = require('node:path');
+
+function executeFolderModule(client, mainDir) {
+  const foldersPath = path.join(process.cwd(), mainDir);
+  const moduleFolder = fs.readdirSync(foldersPath);
+
+  for (const folder of moduleFolder) {
+    const modulePath = path.join(foldersPath, folder);
+    const eventsPathResolve = `${modulePath}/events`;
+    const commandsPathResolve = `${modulePath}/command`;
+    const eventsFiles = fs.readdirSync(eventsPathResolve).filter(file => file.endsWith('.js'));
+    const commandFiles = fs.readdirSync(commandsPathResolve).filter(file => file.endsWith('.js'));
+    for (const file of commandFiles) {
+      try {
+        const filePath = path.join(commandsPathResolve, file);
+        const command = require(filePath);
+
+        if ('data' in command && 'execute' in command) {
+          client.commands.set(command.data.name, command);
+        }
+      }
+      catch {
+        console.error(`[!C] ${file} non caricato`);
+      }
+    }
+    for (const file of eventsFiles) {
+      try {
+        const filePath = path.join(eventsPathResolve, file);
+        const event = require(filePath);
+        client.on(event.name, (...args) => event.execute(...args));
+      }
+      catch (error) {
+        console.log(error)
+        console.error(`[!E] ${file} non caricato`);
+      }
+    }
+  }
+}
+
+module.exports = executeFolderModule;
---
+++ bin/functions/mainEvents.js
@@ -0,0 +1,57 @@
+const { Events, ActivityType } = require('discord.js');
+const executeFolderModule = require('./executeFunctions');
+const { presenceStatusName, botState } = require('../../config.json');
+
+const mainEvents = (client) => {
+  client.on(Events.InteractionCreate, async interaction => {
+    if (!interaction.isChatInputCommand()) return;
+    if (!interaction.guild) return;
+  
+    const command = interaction.client.commands.get(interaction.commandName);
+  
+    if (!command) {
+      console.error(`[WARNING] Il comando ${interaction.commandName} lanciato da ${interaction.user.username} non è stato trovato.`);
+      return;
+    }
+  
+    try {
+      await command.execute(interaction);
+    } catch (error) {
+      console.error(error);
+      if (interaction.replied || interaction.deferred) {
+        await interaction.followUp({ content: 'Abbiamo riscontrato un errore eseguendo questo comando! Contatta un amministratore.', ephemeral: true });
+      } else {
+        await interaction.reply({ content: 'Abbiamo riscontrato un errore eseguendo questo comando! Contatta un amministratore.', ephemeral: true });
+      }
+    }
+  });
+  
+  // ERROR LISTENER
+  client.on(Events.ShardError, error => {
+    console.log(error);
+  })
+  
+  // EVENT LISTNER PER AVVIO DEL BOT
+  
+  client.once(Events.ClientReady, readyClient => {
+    const runtimeConsole = process.env.NODE_ENV === 'production' ? "production" : process.env.NODE_ENV === 'development' ? "development" : "";
+    console.clear();
+    console.log('-------------------------------------');
+    console.log('ALKA HUB BOT v2.0.0 REWORK / BETA');
+    console.log('Author: Elijah (Gabriele Mario Tosto)');
+    console.log('Since: 2024');
+    console.log(`Runtime: ${runtimeConsole}`);
+    console.log('Technology: JavaScript - NodeJs');
+    console.log('-------------------------------------');
+    // FUNZIONI
+    executeFolderModule(client, 'utils');
+  
+  
+    client.user.setPresence({
+      activities: [{ name: presenceStatusName, state: botState, type: ActivityType.Custom }],
+      status: 'online'
+    });
+  })
+}
+
+module.exports = mainEvents;
