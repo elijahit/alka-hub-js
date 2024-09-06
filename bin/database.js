ccb2fd1e0a7ceba3ce3577c67f59f4841b8ccd7e
Elijah <64620855+elijahit@users.noreply.github.com>
2024-09-06 19:46:34 +0200

    EDIT: Rimosse funzioni inutili dal database, da modificare nel codice durante il rework

--- bin/database.js
+++ bin/database.js
@@ -2,9 +2,9 @@ const { AsyncDatabase } = require('promised-sqlite3');
 const database = "./bin/database.db";
 
 
-async function readDb(querysql, parms) {
+async function readDb(querysql, ...parms) {
   const db = await AsyncDatabase.open(database);
-  const result = await db.get(querysql, parms);
+  const result = await db.get(querysql, ...parms);
   await db.close();
   return result;
 }
@@ -16,47 +16,13 @@ async function readDbAll(table) {
   return result;
 }
 
-async function readDbAllWithValue(table, value) {
+async function readDbAllWithParams(table, value) {
   const db = await AsyncDatabase.open(database);
   const result = await db.all(`SELECT ${value} from ${table}`);
   await db.close();
   return result;
 }
 
-async function readDbWith4Params(querysql, ...params) {
-  const db = await AsyncDatabase.open(database);
-  const result = await db.get(querysql, params[0], params[1], params[2], params[3]);
-  await db.close();
-  return result;
-}
-
-async function readDbWith3Params(querysql, ...params) {
-  const db = await AsyncDatabase.open(database);
-  const result = await db.get(querysql, params[0], params[1], params[2]);
-  await db.close();
-  return result;
-}
-
-async function readDbAllWith3Params(querysql, ...params) {
-  const db = await AsyncDatabase.open(database);
-  const result = await db.all(querysql, params[0], params[1], params[2]);
-  await db.close();
-  return result;
-}
-
-async function readDbAllWith2Params(querysql, ...params) {
-  const db = await AsyncDatabase.open(database);
-  const result = await db.all(querysql, params[0], params[1]);
-  await db.close();
-  return result;
-}
-
-async function readDbAllWith1Params(querysql, ...params) {
-  const db = await AsyncDatabase.open(database);
-  const result = await db.all(querysql, params[0]);
-  await db.close();
-  return result;
-}
 
 async function runDb(querysql, ...params) {
   const db = await AsyncDatabase.open(database);
@@ -69,10 +35,5 @@ module.exports = {
   readDb,
   readDbAll,
   runDb,
-  readDbAllWithValue,
-  readDbWith3Params,
-  readDbAllWith2Params,
-  readDbWith4Params,
-  readDbAllWith1Params,
-  readDbAllWith3Params
+  readDbAllWithParams
 };
