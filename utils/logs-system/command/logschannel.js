d39b582c1d3c621df67f64a6db0bfa245ac446d7
Elijah <gabriele.tosto@outlook.com>
2024-11-01 13:04:04 +0100

    REWORK: Comando logschannel

--- utils/logs-system/command/logschannel.js
+++ utils/logs-system/command/logschannel.js
@@ -2,9 +2,10 @@ const { SlashCommandBuilder, EmbedBuilder, ChannelType } = require('discord.js')
 const language = require('../../../languages/languages');
 const { readFileSync, read } = require('fs');
 const { readDb, runDb } = require('../../../bin/database');
-const { errorSendControls, getEmojifromUrl, returnPermission, noInitGuilds } = require('../../../bin/HandlingFunctions');
+const { errorSendControls, returnPermission, noInitGuilds, noEnabledFunc, noHavePermission } = require('../../../bin/HandlingFunctions');
 const colors = require('../../../bin/data/colors');
 const emoji = require('../../../bin/data/emoji');
+const checkFeaturesIsEnabled = require('../../../bin/functions/checkFeaturesIsEnabled');
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -83,36 +84,41 @@ module.exports = {
 		await returnPermission(interaction, "logschannel", async result => {
 			try {
 				if (result) {
-					const checkQuery = `SELECT * FROM logs_system WHERE guilds_id = ?`
-					const checkFeature = await readDb(checkQuery, interaction.guild.id);
-					const embedLog = new EmbedBuilder();
-					//CONTROLLO SE LA ROW E' GIA' PRESENTE NEL DB
-					if (checkFeature) {
-						checkChannelSql = await readDb(`SELECT * FROM logs_system WHERE guilds_id = ?`, interaction.guild.id);
-						if (checkChannelSql[choices]) {
-							customEmoji = emoji.general.falseMaker;
-							runDb(`UPDATE logs_system SET ${choices} = ? WHERE guilds_id = ?`, null, interaction.guild.id);
-							embedLog.setDescription(language_result.commandLogsChannel.description_embed_removed.replace("{0}", choices.split("_")[0]))
-								.setColor(colors.general.error);
+					if (await checkFeaturesIsEnabled(interaction.guild, 1)) {
+						const checkQuery = `SELECT * FROM logs_system WHERE guilds_id = ?`
+						const checkFeature = await readDb(checkQuery, interaction.guild.id);
+						const embedLog = new EmbedBuilder();
+						//CONTROLLO SE LA ROW E' GIA' PRESENTE NEL DB
+						if (checkFeature) {
+							checkChannelSql = await readDb(`SELECT * FROM logs_system WHERE guilds_id = ?`, interaction.guild.id);
+							if (checkChannelSql[choices]) {
+								customEmoji = emoji.general.falseMaker;
+								runDb(`UPDATE logs_system SET ${choices} = ? WHERE guilds_id = ?`, null, interaction.guild.id);
+								embedLog.setDescription(language_result.commandLogsChannel.description_embed_removed.replace("{0}", choices.split("_")[0]))
+									.setColor(colors.general.error);
+							} else {
+								customEmoji = emoji.general.trueMaker
+								runDb(`UPDATE logs_system SET ${choices} = ? WHERE guilds_id = ?`, channel, interaction.guild.id);
+								embedLog
+									.setDescription(language_result.commandLogsChannel.description_embed.replace("{0}", choices.split("_")[0]))
+									.setColor(colors.general.success);
+							}
+
 						} else {
 							customEmoji = emoji.general.trueMaker
-							runDb(`UPDATE logs_system SET ${choices} = ? WHERE guilds_id = ?`, channel, interaction.guild.id);
+							runDb(`INSERT INTO logs_system (guilds_id, ${choices}) VALUES(?, ?)`, interaction.guild.id, channel);
 							embedLog
 								.setDescription(language_result.commandLogsChannel.description_embed.replace("{0}", choices.split("_")[0]))
 								.setColor(colors.general.success);
 						}
-
-					} else {
-						customEmoji = emoji.general.trueMaker
-						runDb(`INSERT INTO logs_system (guilds_id, ${choices}) VALUES(?, ?)`, interaction.guild.id, channel);
 						embedLog
-							.setDescription(language_result.commandLogsChannel.description_embed.replace("{0}", choices.split("_")[0]))
-							.setColor(colors.general.success);
+							.setAuthor({ name: `${language_result.commandLogsChannel.embed_title}`, iconURL: customEmoji })
+							.setFooter({ text: `${language_result.commandLogsChannel.embed_footer}`, iconURL: `${language_result.commandLogsChannel.embed_icon_url}` });
+						await interaction.reply({ embeds: [embedLog], ephemeral: true });
 					}
-					embedLog
-						.setAuthor({ name: `${language_result.commandLogsChannel.embed_title}`, iconURL: customEmoji })
-						.setFooter({ text: `${language_result.commandLogsChannel.embed_footer}`, iconURL: `${language_result.commandLogsChannel.embed_icon_url}` });
-					await interaction.reply({ embeds: [embedLog], ephemeral: true });
+					else {
+						await noEnabledFunc(interaction, language_result.noPermission.description_embed_no_features);
+					}
 				}
 				else {
 					await noHavePermission(interaction, language_result);
