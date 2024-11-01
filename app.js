621754a4337c81f481cee51db4dc89120c3b77ca
Elijah <gabriele.tosto@outlook.com>
2024-11-01 21:10:39 +0100

    SETUP: Beta token

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const { Collection } = require('discord.js');
-const { token, tokenDev } = require('./config.json');
+const { token, tokenDev, tokenBeta } = require('./config.json');
 const { client } = require('./bin/client');
 const mainEvents = require('./bin/functions/mainEvents');
 
@@ -7,6 +7,6 @@ client.commands = new Collection();
 
 mainEvents(client);
 
-const filterToken = process.env.NODE_ENV === 'production' ? token : process.env.NODE_ENV === 'development' ? tokenDev : "";
+const filterToken = process.env.NODE_ENV === 'production' ? token : process.env.NODE_ENV === 'development' ? tokenDev : process.env.NODE_ENV === "beta" ? tokenBeta : "";
 
 client.login(filterToken);
