888e7e3ec9eb3e2ebb34d59cfbf61ad49414d886
Elijah <64620855+elijahit@users.noreply.github.com>
2024-11-03 20:33:19 +0100

    INIT: Iniziato il rework su voiceState

---
+++ utils/autoVoice-system/command/autovoice.js
@@ -0,0 +1,107 @@
+const { SlashCommandBuilder, EmbedBuilder, StringSelectMenuBuilder, StringSelectMenuOptionBuilder, ActionRowBuilder, ChannelType, PermissionFlagsBits } = require('discord.js');
+const language = require('../../../languages/languages');
+const fs = require('fs');
+const { readDb, runDb, readDbAllWith2Params } = require('../../../bin/database');
+const { errorSendControls, getEmoji, returnPermission, noInitGuilds, noHavePermission, noEnabledFunc, getEmojifromUrl } = require('../../../bin/HandlingFunctions');
+const colors = require('../../../bin/data/colors');
+const emoji = require('../../../bin/data/emoji');
+const checkFeaturesIsEnabled = require('../../../bin/functions/checkFeaturesIsEnabled');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('autovoice')
+		.setDescription(' Use this command to initialize the Auto Voice System'),
+	async execute(interaction) {
+
+		// RECUPERO LA LINGUA
+		let data = await language.databaseCheck(interaction.guild.id);
+		const langagues_path = fs.readFileSync(`./languages/autoVoice-system/${data}.json`);
+		const language_result = JSON.parse(langagues_path);
+		// CONTROLLA SE L'UTENTE HA IL PERMESSO PER QUESTO COMANDO
+		await returnPermission(interaction, "autovoice", async result => {
+			try {
+				if (result) {
+					if (await checkFeaturesIsEnabled(interaction.guild, 3)) {
+						const filePath = `./utils/autoVoice-system/${interaction.guild.id}_${interaction.user.id}.json`;
+						if(fs.existsSync(filePath)) {
+							const fileJson = fs.readFileSync(filePath);
+							const fileJsonToObj = JSON.parse(fileJson);
+							try {
+								const deleteChannel = await interaction.guild.channels.fetch(fileJsonToObj.channel_setup);
+								if(deleteChannel) {
+									await deleteChannel.delete();
+								}
+							} catch (error) {
+								// Ignora se non trova il canale
+							}
+							fs.unlinkSync(filePath);
+						}
+						fs.writeFileSync(filePath, '{\n');
+						const selectSetup = new StringSelectMenuBuilder()
+						.setCustomId('autoVoiceSelectMenu')
+						.setPlaceholder(language_result.selectSetup.placeholder)
+						.addOptions(
+							// new StringSelectMenuOptionBuilder() //DA PROGRAMMARE
+							// 	.setLabel(language_result.selectSetup.label_interaction)
+								// 	.setValue("interactionVoice")
+								// 	.setEmoji(channelTextEmoji.id),
+								new StringSelectMenuOptionBuilder()
+								.setLabel(language_result.selectSetup.label_automatic)
+								.setValue("automaticVoice")
+								/*.setEmoji(channelVoiceEmoji.id),*/
+							);
+							
+						const row = new ActionRowBuilder()
+							.addComponents(selectSetup);
+
+						// const customEmoji = await getEmojifromUrl(interaction.client, "utilitysettings");
+						const embedLog = new EmbedBuilder()
+							.setAuthor({ name: `${language_result.selectSetup.embed_title}`/*, iconURL: customEmoji*/ })
+							.setDescription(language_result.selectSetup.description_embed)
+							.setFooter({ text: `${language_result.selectSetup.embed_footer}`, iconURL: `${language_result.selectSetup.embed_icon_url}` })
+							.setColor(0x9ba832);
+
+						// CREO IL CANALE TEMPORANEO DI SETUP
+						const initChannel = await interaction.guild.channels.create({
+							name: 'setup-autovoice',
+							type: ChannelType.GuildText,
+							permissionOverwrites: [
+								{
+									id: interaction.user.id,
+									allow: [PermissionFlagsBits.ViewChannel],
+								},
+								{
+									id: interaction.guild.roles.everyone.id,
+									deny: [PermissionFlagsBits.ViewChannel],
+								}
+							],
+						})
+
+						const embedLogTwo = new EmbedBuilder()
+							.setAuthor({ name: `${language_result.selectSetup.embed_title}`/*, iconURL: customEmoji */})
+							.setDescription(language_result.selectSetup.description_embed_two.replace("{0}", initChannel))
+							.setFooter({ text: `${language_result.selectSetup.embed_footer}`, iconURL: `${language_result.selectSetup.embed_icon_url}` })
+							.setColor(0x03cffc);
+
+						await interaction.reply({ embeds: [embedLogTwo], ephemeral: true });
+						await initChannel.send({ embeds: [embedLog], components: [row] });
+						fs.appendFileSync(filePath, `"guild_id": "${interaction.guild.id}",
+"author_id": "${interaction.user.id}",
+"channel_start": "${interaction.channel.id}",
+"channel_setup": "${initChannel.id}" 
+}`, 'utf8');
+
+					} else {
+						await noEnabledFunc(interaction, language_result.noPermission.description_embed_no_features);
+					}
+				}
+				else {
+					await noHavePermission(interaction, language_result);
+				}
+			}
+			catch (error) {
+				errorSendControls(error, interaction.client, interaction.guild, "\\autoVoice-system\\autovoice.js");
+			}
+		});
+	},
+};
---
+++ utils/autoVoice-system/events/autovoiceinteractions.js
@@ -0,0 +1,268 @@
+const { Events, ChannelSelectMenuBuilder, ActionRowBuilder, ButtonBuilder, ChannelType, EmbedBuilder, PermissionFlagsBits, ButtonStyle, ModalBuilder, TextInputBuilder, TextInputStyle, AttachmentBuilder, StringSelectMenuBuilder, StringSelectMenuOptionBuilder } = require('discord.js');
+const { readFileSync, writeFileSync, unlinkSync } = require('fs');
+const language = require('../../../languages/languages');
+const { readDbAllWith2Params, runDb, readDbWith3Params, readDbWith4Params, readDb } = require('../../../bin/database');
+const { errorSendControls, getEmojifromUrl, getEmoji, returnPermission, noHavePermission, noEnabledFunc } = require('../../../bin/HandlingFunctions');
+
+
+// FUNZIONI
+
+async function endSetup(language_result, interaction) {
+  const check = await readDbAllWith2Params(`SELECT * FROM autovoice_system_creator WHERE authorId = ? AND guildId = ?`, interaction.user.id, interaction.guild.id);
+
+  if (check[0].typeVoice != 2) {
+    const size = interaction.fields.getTextInputValue('sizeSelectAutoVoice');
+    let regex = /^[0-9]+$/;
+    if (regex.test(size)) {
+      await interaction.deferReply();
+      await runDb(`UPDATE autovoice_system_creator SET channelSize = ?, channelId = ? WHERE guildId = ? AND authorId = ?`, size, channel.id, interaction.guild.id, interaction.user.id);
+    }
+  }
+  const category = await interaction.guild.channels.fetch(check[0].categoryId);
+  // CREAZIONE DEL CANALE
+  let channelObject = {};
+
+  // SE IL AUTO VOICE E' AUTOMATICO
+  if (check[0].typeVoice == 2) {
+    // CANALE NUMERICO
+    if (check[0].creatorNickname == 1) {
+      channelObject = {
+        parent: category,
+        name: `changeMe 1`,
+        type: ChannelType.GuildVoice
+      };
+    }
+  }
+
+  const channel = await interaction.guild.channels.create(channelObject);
+  await runDb(`UPDATE autovoice_system_creator SET authorId = ?, channelId = ? WHERE guildId = ? AND authorId = ?`, null, channel.id, interaction.guild.id, interaction.user.id);
+
+  // INVIO MESSAGGIO DI FINE SETUP E CANCELLO IL CANALE
+  await interaction.channel.delete();
+  const setupChannel = await interaction.guild.channels.fetch(check[0].initChannel);
+
+  const customEmoji = await getEmojifromUrl(interaction.client, "utilitysettings")
+  const embedLog = new EmbedBuilder()
+    .setAuthor({ name: `${language_result.endSetup_message.embed_title}`, iconURL: customEmoji })
+    .setDescription(language_result.endSetup_message.description_embed)
+    .setFooter({ text: `${language_result.endSetup_message.embed_footer}`, iconURL: `${language_result.endSetup_message.embed_icon_url}` })
+    .setColor(0x9ba832);
+  await setupChannel.send({ embeds: [embedLog] });
+}
+
+
+module.exports = {
+  name: Events.InteractionCreate,
+  async execute(interaction) {
+    if (!interaction.guild) return;
+    try {
+      // CONTROLLO LINGUA
+      let data = await language.databaseCheck(interaction.guild.id);
+      const langagues_path = readFileSync(`./languages/autoVoice-system/${data}.json`);
+      const language_result = JSON.parse(langagues_path);
+
+      if (interaction.customId == 'autoVoiceSelectMenu') {
+        let value = interaction.values;
+        let typeVoice;
+        await value.forEach(async v => {
+          switch (v) {
+            case "interactionVoice":
+              typeVoice = 1;
+              break;
+            case "automaticVoice":
+              typeVoice = 2;
+              break;
+
+          }
+          await runDb(`UPDATE autovoice_system_creator SET typeVoice = ? WHERE guildId = ? AND authorId = ?`, typeVoice, interaction.guild.id, interaction.user.id);
+        });
+        // RISPONDO ALL'INTERAZIONE ELIMINO IL MESSAGGIO E NE RICREO UNO NUOVO
+        const privateEmoji = await getEmoji(interaction.client, "private");
+        const freeEmoji = await getEmoji(interaction.client, "free_voice");
+        const selectSetup_Creator = new StringSelectMenuBuilder()
+          .setCustomId('autoVoiceSelectMenu_creatorType')
+          .setPlaceholder(language_result.selectSetup_Creator.placeholder)
+          .addOptions(
+            // new StringSelectMenuOptionBuilder() //DA PROGRAMMARE
+            //   .setLabel(language_result.selectSetup_Creator.label_private)
+            //   .setValue("private")
+            //   .setEmoji(privateEmoji.id),
+            new StringSelectMenuOptionBuilder()
+              .setLabel(language_result.selectSetup_Creator.label_free)
+              .setValue("free")
+              .setEmoji(freeEmoji.id),
+          );
+
+        const row = new ActionRowBuilder()
+          .addComponents(selectSetup_Creator);
+
+        const customEmoji = await getEmojifromUrl(interaction.client, "utilitysettings")
+        const embedLog = new EmbedBuilder()
+          .setAuthor({ name: `${language_result.selectSetup_Creator.embed_title}`, iconURL: customEmoji })
+          .setDescription(language_result.selectSetup_Creator.description_embed)
+          .setFooter({ text: `${language_result.selectSetup_Creator.embed_footer}`, iconURL: `${language_result.selectSetup_Creator.embed_icon_url}` })
+          .setColor(0x9ba832);
+        await interaction.message.delete();
+        await interaction.reply({ embeds: [embedLog], components: [row] });
+
+      }
+
+      if (interaction.customId == 'autoVoiceSelectMenu_creatorType') {
+        let value = interaction.values;
+        await value.forEach(async v => {
+          let typeAccess;
+          switch (v) {
+            case "private":
+              typeAccess = 1;
+              break;
+            case "free":
+              typeAccess = 2;
+              break;
+
+          }
+          await runDb(`UPDATE autovoice_system_creator SET creatorType = ? WHERE guildId = ? AND authorId = ?`, typeAccess, interaction.guild.id, interaction.user.id);
+        });
+        // RISPONDO ALL'INTERAZIONE ELIMINO IL MESSAGGIO E NE RICREO UNO NUOVO
+        const voiceNumber = await getEmoji(interaction.client, "voiceNumber");
+        const voiceUser = await getEmoji(interaction.client, "voiceUser");
+        const voiceRandom = await getEmoji(interaction.client, "voiceRandom");
+        const selectSetup_TypeAccess = new StringSelectMenuBuilder()
+          .setCustomId('autoVoiceSelectMenu_accessType')
+          .setPlaceholder(language_result.selectSetup_TypeAccess.placeholder)
+          .addOptions(
+            new StringSelectMenuOptionBuilder()
+              .setLabel(language_result.selectSetup_TypeAccess.label_numeric)
+              .setValue("numeric")
+              .setEmoji(voiceNumber.id),
+            // new StringSelectMenuOptionBuilder() //DA PROGRAMMARE
+            //   .setLabel(language_result.selectSetup_TypeAccess.label_nickname)
+            //   .setValue("nickname")
+            //   .setEmoji(voiceUser.id),
+            // new StringSelectMenuOptionBuilder()
+            //   .setLabel(language_result.selectSetup_TypeAccess.label_random)
+            //   .setValue("random")
+            //   .setEmoji(voiceRandom.id),
+          );
+
+        const row = new ActionRowBuilder()
+          .addComponents(selectSetup_TypeAccess);
+
+        const customEmoji = await getEmojifromUrl(interaction.client, "utilitysettings")
+        const embedLog = new EmbedBuilder()
+          .setAuthor({ name: `${language_result.selectSetup_TypeAccess.embed_title}`, iconURL: customEmoji })
+          .setDescription(language_result.selectSetup_TypeAccess.description_embed)
+          .setFooter({ text: `${language_result.selectSetup_TypeAccess.embed_footer}`, iconURL: `${language_result.selectSetup_TypeAccess.embed_icon_url}` })
+          .setColor(0x9ba832);
+        await interaction.message.delete();
+        await interaction.reply({ embeds: [embedLog], components: [row] });
+
+      }
+
+      if (interaction.customId == "autoVoiceSelectMenu_accessType") {
+        let value = interaction.values;
+        await value.forEach(async v => {
+          let typeNickname;
+          switch (v) {
+            case "numeric":
+              typeNickname = 1;
+              break;
+            case "nickname":
+              typeNickname = 2;
+              break;
+            case "random":
+              typeNickname = 3;
+              break;
+
+          }
+          await runDb(`UPDATE autovoice_system_creator SET creatorNickname = ? WHERE guildId = ? AND authorId = ?`, typeNickname, interaction.guild.id, interaction.user.id);
+        });
+        const selectSetup_Creator = new ChannelSelectMenuBuilder()
+          .setCustomId('autoVoiceSelectMenu_creatorCategory')
+          .setPlaceholder(language_result.select_category.placeholder)
+          .setChannelTypes(ChannelType.GuildCategory);
+
+
+        const row = new ActionRowBuilder()
+          .addComponents(selectSetup_Creator);
+
+        const customEmoji = await getEmojifromUrl(interaction.client, "utilitysettings")
+        const embedLog = new EmbedBuilder()
+          .setAuthor({ name: `${language_result.select_category.embed_title}`, iconURL: customEmoji })
+          .setDescription(language_result.select_category.description_embed)
+          .setFooter({ text: `${language_result.select_category.embed_footer}`, iconURL: `${language_result.select_category.embed_icon_url}` })
+          .setColor(0x9ba832);
+        await interaction.message.delete();
+        await interaction.reply({ embeds: [embedLog], components: [row] });
+      }
+
+
+
+      if (interaction.customId == "autoVoiceSelectMenu_creatorCategory") {
+        const check = await readDbAllWith2Params(`SELECT * FROM autovoice_system_creator WHERE authorId = ? AND guildId = ?`, interaction.user.id, interaction.guild.id);
+
+        // CONTROLLO SE LA CATEGORIA ESISTE GIA'
+        const checkCategory = await readDbAllWith2Params(`SELECT * FROM autovoice_system_creator WHERE guildId = ? AND categoryId = ?`, interaction.guild.id, interaction.values[0]);
+        if (checkCategory.length > 0) {
+          await interaction.channel.delete();
+          const setupChannel = await interaction.guild.channels.fetch(check[0].initChannel);
+
+          const customEmoji = await getEmojifromUrl(interaction.client, "permissiondeny")
+          const embedLog = new EmbedBuilder()
+            .setAuthor({ name: `${language_result.endSetup_message.embed_title}`, iconURL: customEmoji })
+            .setDescription(language_result.endSetup_message.description_embed_errorcategory)
+            .setFooter({ text: `${language_result.endSetup_message.embed_footer}`, iconURL: `${language_result.endSetup_message.embed_icon_url}` })
+            .setColor(0x911010);
+          await setupChannel.send({ embeds: [embedLog] });
+
+          await runDb(`DELETE FROM autovoice_system_creator WHERE guildId = ? AND authorId = ?`, interaction.guild.id, interaction.user.id);
+          return;
+        }
+
+        await runDb(`UPDATE autovoice_system_creator SET categoryId = ? WHERE guildId = ? AND authorId = ?`, interaction.values[0], interaction.guild.id, interaction.user.id);
+
+        if(check[0].typeVoice == 2) {
+          endSetup(language_result, interaction);
+          return;
+        }
+
+        const modal = new ModalBuilder()
+          .setCustomId("autoVoiceModalSelectSize")
+          .setTitle(language_result.select_size.embed_title);
+
+        const sizeSelect = new TextInputBuilder()
+          .setCustomId('sizeSelectAutoVoice')
+          .setLabel(language_result.select_size.label)
+          .setPlaceholder(language_result.select_size.placeholder)
+          .setStyle(TextInputStyle.Short)
+          .setMaxLength(2)
+
+        if (check[0].typeVoice == 1) {
+          const messageSelect = new TextInputBuilder()
+            .setCustomId('messageSelectAutoVoice')
+            .setLabel(language_result.select_size.label_message)
+            .setPlaceholder(language_result.select_size.placeholder_message)
+            .setStyle(TextInputStyle.Paragraph)
+          const row = new ActionRowBuilder().addComponents(sizeSelect);
+          const row2 = new ActionRowBuilder().addComponents(messageSelect);
+          modal.addComponents(row, row2);
+        } else {
+
+          const row = new ActionRowBuilder().addComponents(sizeSelect);
+          modal.addComponents(row);
+        }
+
+
+        await interaction.showModal(modal);
+      }
+
+      if (interaction.customId == "autoVoiceModalSelectSize") {
+        await endSetup(language_result, interaction);
+      }
+
+    }
+    catch (error) {
+      console.log(error)
+      errorSendControls(error, interaction.guild.client, interaction.guild, "\\autoVoice-system\\autovoiceinteractions.js");
+    }
+  },
+};
