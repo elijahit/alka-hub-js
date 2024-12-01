4cd0e5c32c5c2aecc44632eea2e5edfba7ef979c
Gabriele Tosto <g.tosto@flazio.com>
2024-12-01 20:29:36 +0100

    ADD: Modello ORM guild_enabled_features

---
+++ bin/models/guild_enabled_features.js
@@ -0,0 +1,19 @@
+const { database } = require('../database');
+const { Sequelize } = require('sequelize');
+const { Guild } = require('./guilds');
+const { Feature } = require('./features');
+
+const GuildEnabledFeatures = database.define('guild_enabled_features', {
+  guild_id: { type: Sequelize.STRING },
+  feature_id: { type: Sequelize.INTEGER },
+  is_enabled: { type: Sequelize.INTEGER }
+
+}, {tableName: 'guild_enabled_features'});
+
+
+Guild.belongsToMany(Feature, { through: GuildEnabledFeatures, foreignKey: 'guild_id', otherKey: 'feature_id'});
+Feature.belongsToMany(Guild, { through: GuildEnabledFeatures,  foreignKey: 'id', otherKey: 'guild_id' });
+
+module.exports = {
+  GuildEnabledFeatures,
+};
