3bdb361872bb34aa86684c3ce83acfffb9fae711
Gabriele Tosto <g.tosto@flazio.com>
2024-12-02 22:42:37 +0100

    REWORK: checkFeaturesIsEnabled

--- bin/functions/checkFeaturesIsEnabled.js
+++ bin/functions/checkFeaturesIsEnabled.js
@@ -3,11 +3,10 @@ const {checkFeatureEnabled} = require('../service/DatabaseService');
 
 const checkFeaturesIsEnabled = async (guild, features) => {
   let featureIsEnabled = await checkFeatureEnabled(guild, features);
+  if(!featureIsEnabled) return false; 
   featureIsEnabled = featureIsEnabled.get({plain: true}).guilds[0].GuildEnabledFeatures.is_enabled;
   if(featureIsEnabled == 1) return true;
   if(featureIsEnabled == 0) return false;
 }
-
-console.log(checkFeaturesIsEnabled("312", 1).then(v => console.log(v)));
 
 module.exports = checkFeaturesIsEnabled;
