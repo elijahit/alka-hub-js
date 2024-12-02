999b40e84d27c29114437443fb7528b8bf8b9f75
Gabriele Tosto <g.tosto@flazio.com>
2024-12-02 20:20:18 +0100

    ADD: Aggiunta documentazione su DatabaseService.js

--- bin/service/DatabaseService.js
+++ bin/service/DatabaseService.js
@@ -4,12 +4,21 @@
 */
 
 const { addUser: addUserGuild } = require('../repository/UserGuild');
-const { findByUserId: findUserById } = require('../repository/User');
-const { findByGuildId: findGuildById } = require('../repository/Guild');
-const { findByPermissionName } = require('../repository/Permissions');
-const { findByHashName, findAllCorrespondenceByHashName } = require('../repository/Hash');
+// addUserGuild - > Permette di aggiungere un utente rispettando le relazioni (User e Guild)
 
-findAllCorrespondenceByHashName("features").then(v => console.log(v)); // <- da fixare
+const { findByUserId: findUserById } = require('../repository/User');
+// findUserById -> Permette di cercare un utente tramite discordId
+
+const { findByGuildId: findGuildById } = require('../repository/Guild');
+// findGuildById -> Permette di cercare un una guild tramite discordId
+
+const { findByPermissionName } = require('../repository/Permissions');
+// findByPermissionName -> Permette di cercare un permesso tramite il suo nome
+
+const { findByHashName, findAllCorrespondenceByHashName } = require('../repository/Hash');
+// findByHashName -> Permette di cercare un permesso tramite il suo hashName
+// findAllCorrespondenceByHashName -> Permette di cercare un permesso tramite hashName e restituisce tutte le sue corrispondenze della tabella permissions
+
 
 module.exports = {
   addUserGuild,
