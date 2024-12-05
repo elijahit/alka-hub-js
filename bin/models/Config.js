857e04e028984e2a282b4422d9e2ab727bdac2c5
Gabriele Tosto <g.tosto@flazio.com>
2024-12-05 23:13:19 +0100

    ADD: Aggiunto process.exit in caso di bot disattivato

--- bin/models/Config.js
+++ bin/models/Config.js
@@ -3,6 +3,7 @@ const { Sequelize } = require('sequelize');
 
 const Config = database.define('configs', {
   name: { type: Sequelize.STRING },
+  owner_discord_id: { type: Sequelize.STRING },
   json: { type: Sequelize.STRING },
   isActive: { type: Sequelize.INTEGER },
   premium: { type: Sequelize.INTEGER }
