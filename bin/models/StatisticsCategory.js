cbcca398a044b8520ae68786e917d6adf05ba4f3
Gabriele Tosto <g.tosto@flazio.com>
2024-12-27 20:18:55 +0100

    REFACTOR: Update StatisticsCategory model to replace channel_id and channel_name with category_id

--- bin/models/StatisticsCategory.js
+++ bin/models/StatisticsCategory.js
@@ -12,9 +12,7 @@ const { Sequelize } = require('sequelize');
 const StatisticsCategory = database.define('statistics_category', {
   config_id: { type: Sequelize.INTEGER },
   guild_id: { type: Sequelize.STRING },
-  channel_id: { type: Sequelize.STRING },
-  channel_name: { type: Sequelize.INTEGER },
-  type: { type: Sequelize.STRING }
+  category_id: { type: Sequelize.STRING }
 }, {tableName: 'statistics_category'});
 
 module.exports = {
