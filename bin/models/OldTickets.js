d2ec4d1eca437130569b8b97017f4e92284fbae1
Gabriele Tosto <g.tosto@flazio.com>
2025-02-23 15:45:05 +0100

    Add OldTickets and OldTicketsMessage models; refactor ticket command and clean up backup files

---
+++ bin/models/OldTickets.js
@@ -0,0 +1,25 @@
+// Code: OldTickets - bin/models/OldTickets.js
+// Author: Gabriele Mario Tosto <g.tosto@flazio.com> - Alka Hub 2024/25
+/**
+ * @file OldTickets.js
+ * @module OldTickets
+ * @description Contiene il modello OldTickets
+ */
+
+const { database } = require('../database');
+const { Sequelize } = require('sequelize');
+
+const OldTickets = database.define('old_ticket_tickets', {
+  config_id: { type: Sequelize.INTEGER },
+  guild_id: { type: Sequelize.STRING },
+  author_id: { type: Sequelize.STRING },
+  channel_id: { type: Sequelize.STRING },
+  message_id: { type: Sequelize.STRING },
+  ticketPrefix: { type: Sequelize.STRING },
+  ticketSystemMessage_id: { type: Sequelize.STRING },
+});
+
+
+module.exports = {
+  OldTickets,
+};
